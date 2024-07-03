40e75bf91fc7444058c8b2a7c71495ae45100b47
sung-silver <jinna0319@gmail.com>
2024-07-03 16:45:20 +0900

    [ADD] 대학 시험 파일 조회 성공 타입 추가

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/exception/UniversityExamSuccessType.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/exception/UniversityExamSuccessType.java
@@ -11,7 +11,9 @@ public enum UniversityExamSuccessType implements SuccessType {
      * 200 OK
      */
     GET_UNIVERSITY_EXAM_SUCCESS(HttpStatus.OK, "대학 시험 정보 조회에 성공했습니다"),
+    // TODO: 이미지 조회에서 PDF 조회로 변경되면 지워야 함
     GET_UNIVERSITY_EXAM_IMAGE_SUCCESS(HttpStatus.OK, "대학 시험 이미지 조회에 성공했습니다"),
+    GET_UNIVERSITY_EXAM_FILE_SUCCESS(HttpStatus.OK, "대학 시험 파일 조회에 성공했습니다"),
     GET_UNIVERSITY_EXAM_IMAGE_AND_ANSWER_SUCCESS(HttpStatus.OK, "대학 시험 이미지 및 해제 PDF 조회에 성공했습니다");
 
 
