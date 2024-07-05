f24ea533bf3ba127ff9c43cba9239ce5ec1ab925
sung-silver <jinna0319@gmail.com>
2024-07-05 16:06:50 +0900

    [FEAT] swagger description 수정: 요청 -> 응답

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/controller/dto/response/UniversityExamFileResponseDTO.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/controller/dto/response/UniversityExamFileResponseDTO.java
@@ -2,8 +2,9 @@ package com.nonsoolmate.nonsoolmateServer.domain.university.controller.dto.respo
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
+@Schema(name = "UniversityExamFileResponseDTO", description = "대학시험 문제 PDF 조회 응답 DTO")
 public record UniversityExamFileResponseDTO(
-        @Schema(description = "시험 문제 PDF URL") String universityExamFileUrl
+        @Schema(description = "시험 문제 PDF URL") String universityExamUrl
 ) {
     public static UniversityExamFileResponseDTO of(final String universityExamFileUrl) {
         return new UniversityExamFileResponseDTO(universityExamFileUrl);
