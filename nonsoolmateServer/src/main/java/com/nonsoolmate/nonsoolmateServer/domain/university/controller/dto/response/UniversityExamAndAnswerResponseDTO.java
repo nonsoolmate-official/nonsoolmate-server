230cdb4eb3978a3bae94e7b7a19cfcb3451428b2
sung-silver <jinna0319@gmail.com>
2024-07-05 16:23:06 +0900

    [FIX] UniversityExamAndAnswerResponseDTO 필드명 수정

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/controller/dto/response/UniversityExamAndAnswerResponseDTO.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/controller/dto/response/UniversityExamAndAnswerResponseDTO.java
@@ -7,12 +7,12 @@ import java.util.List;
 @Schema(name = "UniversityExamAndAnswerResponseDTO", description = "대학시험 문제PDF_해제PDF 조회 응답 DTO")
 public record UniversityExamAndAnswerResponseDTO(
         @Schema(description = "시험 이름(대학 + 시험 년도 + 시험 이름)", example = "2023 중앙대학교 경영경제 1") String universityExamName,
-        @Schema(description = "시험 문제 이미지 리스트") String universityExamFileUrl,
+        @Schema(description = "시험 문제 이미지 리스트") String universityExamUrl,
         @Schema(description = "이미지 해제 PDF URL") String universityExamAnswerUrl
 ) {
     public static UniversityExamAndAnswerResponseDTO of(final String universityExamName,
-                                                        final String universityExamFileUrl,
+                                                        final String universityExamUrl,
                                                         final String universityExamAnswerUrl) {
-        return new UniversityExamAndAnswerResponseDTO(universityExamName, universityExamFileUrl, universityExamAnswerUrl);
+        return new UniversityExamAndAnswerResponseDTO(universityExamName, universityExamUrl, universityExamAnswerUrl);
     }
 }
