c135c49413df1ab59313284b862d5377762b25c5
sung-silver <jinna0319@gmail.com>
2024-07-08 17:10:00 +0900

    [REFACTOR] UniversityExamImageRepository -> ExamImageRepository 변경

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/repository/ExamImageRepository.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/repository/ExamImageRepository.java
@@ -8,10 +8,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UniversityExamImageRepository extends JpaRepository<ExamImage, Long> {
-    Page<ExamImage> findAllByUniversityExamOrderByPageAsc(Exam exam,
-                                                                    Pageable pageable);
+public interface ExamImageRepository extends JpaRepository<ExamImage, Long> {
+    Page<ExamImage> findAllByExamOrderByExamImageIdAscOrderByPageAsc(Exam exam, Pageable pageable);
 
-    List<ExamImage> findAllByUniversityExamOrderByPageAsc(Exam exam);
+    List<ExamImage> findAllByExamOrderByExamImageIdAsc(Exam exam);
 
 }
