f68a457e9a8ce4de19554e3894c14266f544223c
mikekks <thdalsrb79@naver.com>
2024-07-10 14:57:11 +0900

    [FEAT] 내 프로필 조회 API 구현

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/member/exception/MemberSuccessType.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/member/exception/MemberSuccessType.java
@@ -9,7 +9,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum MemberSuccessType implements SuccessType {
 	GET_MEMBER_NAME_SUCCESS(HttpStatus.OK, "이름 조회에 성공하였습니다."),
-	GET_MEMBER_TICKET_SUCCESS(HttpStatus.OK, "첨삭권 개수 조회에 성공하였습니다.");
+	GET_MEMBER_TICKET_SUCCESS(HttpStatus.OK, "첨삭권 개수 조회에 성공하였습니다."),
+	GET_MEMBER_PROFILE_SUCCESS(HttpStatus.OK, "프로필 조회에 성공하였습니다.");
 
 	private final HttpStatus status;
 	private final String message;
