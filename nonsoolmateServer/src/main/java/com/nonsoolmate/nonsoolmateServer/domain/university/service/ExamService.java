7dd13acfc81d573cc5b04a29e3e6f72925c3c4e5
mikekks <thdalsrb79@naver.com>
2024-07-22 15:42:45 +0900

    [CHORE] 이미지 -> pdf 변경으로 인한 수정

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/service/ExamService.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/service/ExamService.java
@@ -1,25 +1,16 @@
 package com.nonsoolmate.nonsoolmateServer.domain.university.service;
 
+import static com.nonsoolmate.nonsoolmateServer.domain.university.exception.ExamExceptionType.*;
 import static com.nonsoolmate.nonsoolmateServer.external.aws.FolderName.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.nonsoolmate.nonsoolmateServer.domain.university.controller.dto.response.ExamAndAnswerResponseDTO;
-import com.nonsoolmate.nonsoolmateServer.domain.university.controller.dto.response.ExamImageAndAnswerResponseDTO;
-import com.nonsoolmate.nonsoolmateServer.domain.university.controller.dto.response.ExamImageResponseDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.university.controller.dto.response.ExamInfoResponseDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.university.controller.dto.response.ExamUrlResponseDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.university.entity.Exam;
-import com.nonsoolmate.nonsoolmateServer.domain.university.entity.ExamImage;
 import com.nonsoolmate.nonsoolmateServer.domain.university.exception.ExamException;
-import com.nonsoolmate.nonsoolmateServer.domain.university.exception.ExamExceptionType;
-import com.nonsoolmate.nonsoolmateServer.domain.university.repository.ExamImageRepository;
 import com.nonsoolmate.nonsoolmateServer.domain.university.repository.ExamRepository;
 import com.nonsoolmate.nonsoolmateServer.external.aws.service.CloudFrontService;
 
@@ -30,73 +21,35 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ExamService {
 	private final ExamRepository examRepository;
-	private final ExamImageRepository examImageRepository;
 	private final CloudFrontService cloudFrontService;
 
 	public ExamInfoResponseDTO getExamInfo(final Long examId) {
-		Exam exam = examRepository.findByExamId(examId)
-			.orElseThrow(() -> new ExamException(
-				ExamExceptionType.INVALID_EXAM));
+		Exam exam = getExam(examId);
 		String examName = exam.getExamFullName();
 		return ExamInfoResponseDTO.of(exam.getExamId(),
 			examName,
 			exam.getExamTimeLimit());
 	}
 
-	public ExamUrlResponseDTO getExamFile(final Long id) {
-		Exam exam = examRepository.findByExamId(id)
-			.orElseThrow(() -> new ExamException(
-				ExamExceptionType.INVALID_EXAM));
+	public ExamUrlResponseDTO getExamFile(final Long examId) {
+		Exam exam = getExam(examId);
 		return ExamUrlResponseDTO.of(cloudFrontService.createPreSignedGetUrl(EXAM_FILE_FOLDER_NAME,
 			exam.getExamFileName()));
 	}
 
-	public Page<ExamImageResponseDTO> getExamImages(final Long id, final Pageable pageable) {
-		Exam exam = examRepository.findByExamId(id)
-			.orElseThrow(() -> new ExamException(
-				ExamExceptionType.INVALID_EXAM));
-		Page<ExamImage> universityExamImages = examImageRepository.findAllByExamOrderByExamImageIdAscPageAsc(
-			exam,
-			pageable);
-		return universityExamImages.map(image ->
-			ExamImageResponseDTO.of(cloudFrontService.createPreSignedGetUrl(EXAM_IMAGE_FOLDER_NAME,
-				image.getExamImageFileName())));
-	}
-
-	@SuppressWarnings("checkstyle:RegexpSinglelineJava")
-	public ExamImageAndAnswerResponseDTO getExamImageAndAnswer(Long examId) {
-		Exam exam = examRepository.findByExamId(examId)
-			.orElseThrow(() -> new ExamException(
-				ExamExceptionType.INVALID_EXAM));
-
-		String examAnswerUrl = cloudFrontService.createPreSignedGetUrl(EXAM_ANSWER_FOLDER_NAME,
-			exam.getExamAnswerFileName());
-		List<ExamImageResponseDTO> examImageUrls = new ArrayList<>();
-
-		List<ExamImage> examImages = examImageRepository.findAllByExamOrderByExamImageIdAsc(
-			exam);
-
-		examImages.stream().forEach(examImage -> {
-			String preSignedGetUrl = cloudFrontService.createPreSignedGetUrl(EXAM_IMAGE_FOLDER_NAME,
-				examImage.getExamImageFileName());
-			examImageUrls.add(
-				ExamImageResponseDTO.of(preSignedGetUrl));
-		});
-
-		return ExamImageAndAnswerResponseDTO.of(
-			exam.getExamFullName()
-			, examImageUrls, examAnswerUrl);
-	}
-
 	public ExamAndAnswerResponseDTO getExamAndAnswer(final Long examId) {
-		Exam exam = examRepository.findByExamId(examId)
-			.orElseThrow(() -> new ExamException(ExamExceptionType.INVALID_EXAM));
+		Exam exam = getExam(examId);
 		String examUrl = cloudFrontService.createPreSignedGetUrl(EXAM_FILE_FOLDER_NAME, exam.getExamFileName());
 		String universityAnswerUrl = cloudFrontService.createPreSignedGetUrl(EXAM_ANSWER_FOLDER_NAME,
 			exam.getExamAnswerFileName());
 		return ExamAndAnswerResponseDTO.of(
 			exam.getExamFullName(),
 			examUrl, universityAnswerUrl);
+	}
+
+	private Exam getExam(final Long examId) {
+		return examRepository.findByExamId(examId)
+			.orElseThrow(() -> new ExamException(INVALID_EXAM));
 	}
 
 }
