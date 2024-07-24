abb37baf74fe3da9ccacbc21760250ca41d02854
mikekks <thdalsrb79@naver.com>
2024-07-24 15:00:56 +0900

    [CHORE] exam-image 삭제

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/aws/FolderName.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/external/aws/FolderName.java
@@ -4,7 +4,5 @@ public abstract class FolderName {
 	public static final String EXAM_ANSWER_FOLDER_NAME = "exam-answer/";
 	public static final String EXAM_RESULT_FOLDER_NAME = "exam-result/";
 	public static final String EXAM_SHEET_FOLDER_NAME = "exam-sheet/";
-	// TODO: exma-image -> 사용안해도 되는 기간에 삭제
-	public static final String EXAM_IMAGE_FOLDER_NAME = "exam-image/";
 	public static final String EXAM_FILE_FOLDER_NAME = "exam-pdf/";
 }
