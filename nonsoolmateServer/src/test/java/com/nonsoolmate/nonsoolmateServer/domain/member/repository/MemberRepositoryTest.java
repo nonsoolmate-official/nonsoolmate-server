4f98978746194c69a4993563b419e4323db00ba2
sung-silver <jinna0319@gmail.com>
2024-08-06 23:23:28 +0900

    [TEST] MemberRepositoryTest - saveMemberDuplicate() 테스트 코드 작성

---
+++ nonsoolmateServer/src/test/java/com/nonsoolmate/nonsoolmateServer/domain/member/repository/MemberRepositoryTest.java
@@ -0,0 +1,75 @@
+package com.nonsoolmate.nonsoolmateServer.domain.member.repository;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.nonsoolmate.nonsoolmateServer.domain.member.entity.Member;
+import com.nonsoolmate.nonsoolmateServer.domain.member.entity.enums.PlatformType;
+import com.nonsoolmate.nonsoolmateServer.domain.member.entity.enums.Role;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@DataJpaTest
+@ActiveProfiles("test")
+class MemberRepositoryTest {
+	@Autowired
+	private MemberRepository memberRepository;
+
+	private Member member;
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@BeforeEach
+	void setup() {
+		member = Member.builder()
+			.email("test@example.com")
+			.name("euna")
+			.platformType(PlatformType.NAVER)
+			.role(Role.USER)
+			.platformId("12345")
+			.birthYear("2001")
+			.gender("F")
+			.phoneNumber("010-1234-5678")
+			.build();
+	}
+
+	@AfterEach
+	void tearDown() {
+		memberRepository.deleteAllInBatch();
+	}
+
+	@Test
+	@DisplayName("동일한 회원에 대한 회원가입 요청이 2번 오는 경우 무결성 에러가 발생한다")
+	void saveMemberDuplicate() {
+		// given
+		Member duplicateMember = Member.builder()
+			.email("test@example.com")
+			.name("euna")
+			.platformType(PlatformType.NAVER)
+			.role(Role.USER)
+			.platformId("12345")
+			.birthYear("2001")
+			.gender("F")
+			.phoneNumber("010-1234-5678")
+			.build();
+		memberRepository.save(member);
+
+		// when, then
+		assertThatThrownBy(() -> memberRepository.saveAndFlush(duplicateMember))
+			.isInstanceOf(DataIntegrityViolationException.class);
+		entityManager.clear();
+
+		assertThat(memberRepository.count()).isEqualTo(1);
+	}
+
+}
