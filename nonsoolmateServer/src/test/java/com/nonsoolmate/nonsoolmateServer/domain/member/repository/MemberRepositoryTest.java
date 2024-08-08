74b968eb690edde56a1a128b0c7d82e7a3592551
sung-silver <jinna0319@gmail.com>
2024-08-08 19:50:25 +0900

    [TEST] MemberRepositoryTest - saveMemberConcurrent() 테스트 코드 작성

--- nonsoolmateServer/src/test/java/com/nonsoolmate/nonsoolmateServer/domain/member/repository/MemberRepositoryTest.java
+++ nonsoolmateServer/src/test/java/com/nonsoolmate/nonsoolmateServer/domain/member/repository/MemberRepositoryTest.java
@@ -1,6 +1,14 @@
 package com.nonsoolmate.nonsoolmateServer.domain.member.repository;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -49,7 +57,7 @@ class MemberRepositoryTest {
 	}
 
 	@Test
-	@DisplayName("동일한 회원에 대한 회원가입 요청이 2번 오는 경우 무결성 에러가 발생한다")
+	@DisplayName("동일한 회원에 대한 회원가입 요청이 오는 경우 무결성 에러가 발생한다")
 	void saveMemberDuplicate() {
 		// given
 		Member duplicateMember = Member.builder()
@@ -70,6 +78,52 @@ class MemberRepositoryTest {
 		entityManager.clear();
 
 		assertThat(memberRepository.count()).isEqualTo(1);
+	}
+
+	@Test
+	@DisplayName("동일한 회원에 대한 회원가입 요청이 동시에 오는 경우 무결성 에러가 발생한다")
+	void saveMemberConcurrent() throws InterruptedException {
+		Member duplicateMember = Member.builder()
+			.email("test@example.com")
+			.name("euna")
+			.platformType(PlatformType.NAVER)
+			.role(Role.USER)
+			.platformId("12345")
+			.birthYear("2001")
+			.gender("F")
+			.phoneNumber("010-1234-5678")
+			.build();
+		CountDownLatch latch = new CountDownLatch(2);
+		ExecutorService executor = Executors.newFixedThreadPool(2);
+
+		Callable<Void> task = () -> {
+			try {
+				memberRepository.saveAndFlush(duplicateMember);
+			} finally {
+				latch.countDown();
+			}
+			return null;
+		};
+
+		Future<Void> future1 = executor.submit(task);
+		Future<Void> future2 = executor.submit(task);
+
+		latch.await();
+
+		executor.shutdown();
+
+		assertThatThrownBy(() -> {
+			try {
+				future1.get();
+				future2.get();
+			} catch (ExecutionException e) {
+				throw e.getCause();
+			}
+		}).isInstanceOf(DataIntegrityViolationException.class);
+
+		entityManager.clear();
+
+		assertEquals(1, memberRepository.count());
 	}
 
 }
