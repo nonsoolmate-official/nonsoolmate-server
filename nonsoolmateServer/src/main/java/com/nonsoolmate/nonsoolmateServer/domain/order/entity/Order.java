186f6128fd98d7649f890c8ddd27904fd3f0d2f9
sung-silver <jinna0319@gmail.com>
2024-08-15 09:44:26 +0900

    [FEAT] Order 엔티티에 getter 추가

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/order/entity/Order.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/order/entity/Order.java
@@ -9,10 +9,12 @@ import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Order {
 	@Id
 	private String orderId;
