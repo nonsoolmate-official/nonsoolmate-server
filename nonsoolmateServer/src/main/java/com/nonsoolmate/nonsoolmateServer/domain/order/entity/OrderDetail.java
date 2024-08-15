5987a00cb95426b7ad8a524558f82df6364bb49c
sung-silver <jinna0319@gmail.com>
2024-08-15 10:17:52 +0900

    [FEAT] Order 엔티티 이름 변경(예약어 이슈) 및 Transaction 연관관계 1:N으로 수정

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/order/entity/OrderDetail.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/order/entity/OrderDetail.java
@@ -1,11 +1,7 @@
 package com.nonsoolmate.nonsoolmateServer.domain.order.entity;
 
-import com.nonsoolmate.nonsoolmateServer.domain.payment.entity.TransactionDetail;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -15,28 +11,21 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-public class Order {
+// TODO: Order가 customerKey를 가진다면, Transaction은 가지지 않아도 되지 않는가에 대하여
+public class OrderDetail {
 	@Id
 	private String orderId;
 
 	@NotNull
 	private String orderName;
 
-	@OneToOne
-	@JoinColumn(name = "transactionKey")
-	TransactionDetail transactionDetail;
-
 	@NotNull
 	private long amount;
 
 	@Builder
-	public Order(final String orderId, final String orderName, final long amount) {
+	public OrderDetail(final String orderId, final String orderName, final long amount) {
 		this.orderId = orderId;
 		this.orderName = orderName;
 		this.amount = amount;
-	}
-
-	public void updateTransactionDetail(final TransactionDetail transactionDetail) {
-		this.transactionDetail = transactionDetail;
 	}
 }
