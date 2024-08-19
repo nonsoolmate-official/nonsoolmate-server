f25562850ce0af4331faad4c6ad31893e6918af4
sung-silver <jinna0319@gmail.com>
2024-08-19 14:44:04 +0900

    [FEAT] Order 엔티티에 customerKey 필드 추가

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/order/entity/OrderDetail.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/order/entity/OrderDetail.java
@@ -1,7 +1,14 @@
 package com.nonsoolmate.nonsoolmateServer.domain.order.entity;
 
+import com.nonsoolmate.nonsoolmateServer.domain.member.entity.Member;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -11,21 +18,27 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-// TODO: Order가 customerKey를 가진다면, Transaction은 가지지 않아도 되지 않는가에 대하여
 public class OrderDetail {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private String orderId;
 
 	@NotNull
 	private String orderName;
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@NotNull
+	@JoinColumn(name = "customer_key")
+	private Member member;
+
 	@NotNull
 	private long amount;
 
 	@Builder
-	public OrderDetail(final String orderId, final String orderName, final long amount) {
+	public OrderDetail(final String orderId, final String orderName, final Member member, final long amount) {
 		this.orderId = orderId;
 		this.orderName = orderName;
+		this.member = member;
 		this.amount = amount;
 	}
 }
