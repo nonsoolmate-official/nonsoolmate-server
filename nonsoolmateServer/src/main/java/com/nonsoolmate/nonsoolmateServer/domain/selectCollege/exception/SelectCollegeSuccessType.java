d62f34ea01f5998da6938b829f7c8e4a5fbc8de4
sung-silver <jinna0319@gmail.com>
2024-08-21 17:27:24 +0900

    [FEAT] Swagger 및 성공&예외 타입에서 목표 대학을 목표 단과 대학으로 변경

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectCollege/exception/SelectCollegeSuccessType.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/selectCollege/exception/SelectCollegeSuccessType.java
@@ -8,9 +8,9 @@ import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum SelectCollegeSuccessType implements SuccessType {
-	GET_SELECT_UNIVERSITIES_SUCCESS(HttpStatus.OK, "목표 대학 조회에 성공하였습니다."),
-	GET_SELECT_UNIVERSITY_EXAMS_SUCCESS(HttpStatus.OK, "목표 대학 시험 리스트 조회에 성공하였습니다."),
-	PATCH_SELECT_UNIVERSITIES_SUCCESS(HttpStatus.OK, "목표 대학 수정에 성공하였습니다.");
+	GET_SELECT_COLLEGES_SUCCESS(HttpStatus.OK, "목표 단과 대학 조회에 성공하였습니다."),
+	GET_SELECT_COLLEGE_EXAMS_SUCCESS(HttpStatus.OK, "목표 단과 대학 시험 리스트 조회에 성공하였습니다."),
+	PATCH_SELECT_COLLEGES_SUCCESS(HttpStatus.OK, "목표 단과 대학 수정에 성공하였습니다.");
 
 	private final HttpStatus status;
 	private final String message;
