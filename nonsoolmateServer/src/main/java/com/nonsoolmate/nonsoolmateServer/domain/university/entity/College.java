516d0e75ab2198e5f05320cfabf7d405c789e77b
sung-silver <jinna0319@gmail.com>
2024-08-21 15:53:42 +0900

    [FEAT] University <-> College 분리

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/entity/College.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/entity/College.java
@@ -1,22 +1,27 @@
 package com.nonsoolmate.nonsoolmateServer.domain.university.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class University {
+@Getter
+public class College {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long universityId;
+	private Long collegeId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private University university;
 
 	@NotNull
-	private String universityName;
+	private String collegeName;
 }
