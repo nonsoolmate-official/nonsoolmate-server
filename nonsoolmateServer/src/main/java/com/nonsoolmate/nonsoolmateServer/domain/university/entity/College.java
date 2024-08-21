258ccf5cb164d9f42de47c4676084fb8aa11a72e
sung-silver <jinna0319@gmail.com>
2024-08-21 15:54:25 +0900

    [FEAT] College 연관관계 매핑에 컬럼명 명시

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/entity/College.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/entity/College.java
@@ -5,6 +5,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -20,6 +21,7 @@ public class College {
 	private Long collegeId;
 
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "university_id")
 	private University university;
 
 	@NotNull
