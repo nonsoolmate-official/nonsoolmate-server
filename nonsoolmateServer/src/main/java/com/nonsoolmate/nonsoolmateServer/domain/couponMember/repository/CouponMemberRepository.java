10d2d6c3854ec45ed55c6d9866e2557f7d1e3430
mikekks <thdalsrb79@naver.com>
2024-08-22 16:30:48 +0900

    [FEAT] 중복 방지 방어 코드 추가

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/couponMember/repository/CouponMemberRepository.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/couponMember/repository/CouponMemberRepository.java
@@ -1,10 +1,12 @@
 package com.nonsoolmate.nonsoolmateServer.domain.couponMember.repository;
 
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nonsoolmate.nonsoolmateServer.domain.couponMember.entity.CouponMember;
 
 public interface CouponMemberRepository extends JpaRepository<CouponMember, Long>, CouponMemberCustomRepository {
-
+	Optional<CouponMember> findByCouponId(Long couponId);
 }
