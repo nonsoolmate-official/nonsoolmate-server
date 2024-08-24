c45299224581357183a279dea941ac222a5f0864
sung-silver <jinna0319@gmail.com>
2024-08-24 15:11:56 +0900

    [FEAT] EnumType에 대하여 Enumerated 어노테이션 적용

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/member/entity/Membership.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/member/entity/Membership.java
@@ -7,6 +7,8 @@ import com.nonsoolmate.nonsoolmateServer.domain.member.entity.enums.MembershipSt
 import com.nonsoolmate.nonsoolmateServer.domain.member.entity.enums.MembershipType;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -26,11 +28,13 @@ public class Membership extends BaseTimeEntity {
 	@OneToOne
 	Member member;
 
+	@Enumerated(EnumType.STRING)
 	MembershipType membershipType;
 
 	LocalDateTime startDate;
 
 	LocalDateTime endDate;
 
+	@Enumerated(EnumType.STRING)
 	MembershipStatus status;
 }
