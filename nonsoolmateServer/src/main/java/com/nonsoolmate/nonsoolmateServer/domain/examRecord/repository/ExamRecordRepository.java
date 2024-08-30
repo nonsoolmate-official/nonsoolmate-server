62532655d5c9ae25f5d7da4b30467270ccdbcd8a
sung-silver <jinna0319@gmail.com>
2024-08-30 15:08:30 +0900

    [DEL] 작동되지 않는 메서드 삭제

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/examRecord/repository/ExamRecordRepository.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/examRecord/repository/ExamRecordRepository.java
@@ -1,7 +1,5 @@
 package com.nonsoolmate.nonsoolmateServer.domain.examRecord.repository;
 
-import static com.nonsoolmate.nonsoolmateServer.domain.examRecord.exception.ExamRecordExceptionType.*;
-
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,13 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.nonsoolmate.nonsoolmateServer.domain.examRecord.entity.ExamRecord;
 import com.nonsoolmate.nonsoolmateServer.domain.member.entity.Member;
 import com.nonsoolmate.nonsoolmateServer.domain.university.entity.Exam;
-import com.nonsoolmate.nonsoolmateServer.domain.university.exception.ExamException;
 
 public interface ExamRecordRepository extends JpaRepository<ExamRecord, Long> {
 	List<ExamRecord> findByExamAndMember(Exam university, Member member);
-
-	default ExamRecord findByExamAndMemberOrElseThrowException(Exam university, Member member) {
-		return findByExamAndMember(university, member).orElseThrow(
-			() -> new ExamException(NOT_FOUND_UNIVERSITY_EXAM_RECORD));
-	}
 }
