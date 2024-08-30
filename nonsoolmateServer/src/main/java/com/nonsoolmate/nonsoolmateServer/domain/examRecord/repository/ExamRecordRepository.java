4c8b74fe7dca467a88b08939ade82c01db4cb936
sung-silver <jinna0319@gmail.com>
2024-08-30 15:20:59 +0900

    [FEAT] EditingType에 따라 ExamRecord 조회하는 메서드 추가

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/examRecord/repository/ExamRecordRepository.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/examRecord/repository/ExamRecordRepository.java
@@ -1,13 +1,17 @@
 package com.nonsoolmate.nonsoolmateServer.domain.examRecord.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nonsoolmate.nonsoolmateServer.domain.examRecord.entity.ExamRecord;
+import com.nonsoolmate.nonsoolmateServer.domain.examRecord.entity.enums.EditingType;
 import com.nonsoolmate.nonsoolmateServer.domain.member.entity.Member;
 import com.nonsoolmate.nonsoolmateServer.domain.university.entity.Exam;
 
 public interface ExamRecordRepository extends JpaRepository<ExamRecord, Long> {
 	List<ExamRecord> findByExamAndMember(Exam university, Member member);
+
+	Optional<ExamRecord> findByExamAndMemberAndEditingType(Exam exam, Member member, EditingType editingType);
 }
