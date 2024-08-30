1828b0b8d40c5308526478c51b8d32392b4ec8ef
sung-silver <jinna0319@gmail.com>
2024-08-30 14:15:37 +0900

    [FEAT] ExamResultStatus에 재첨삭 플로우 고려하여 상태값 추가

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/examRecord/entity/enums/ExamResultStatus.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/examRecord/entity/enums/ExamResultStatus.java
@@ -6,7 +6,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum ExamResultStatus {
-	ONGOING("첨삭 진행 중"), FINISH("첨삭 완료");
+	REVIEW_ONGOING("첨삭 진행 중"),
+	REVIEW_FINISH("첨삭 완료"),
+	RE_REVIEW_ONGOING("재첨삭 진행 중"),
+	RE_REVIEW_FINISH("재첨삭 완료");
 
 	private final String status;
 }
