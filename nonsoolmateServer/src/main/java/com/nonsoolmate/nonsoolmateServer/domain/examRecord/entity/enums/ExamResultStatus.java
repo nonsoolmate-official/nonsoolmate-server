1474bf66e454f603b8e9cc5b5623f5bfb2b99a71
sung-silver <jinna0319@gmail.com>
2024-08-30 18:38:04 +0900

    [FEAT] ExamResultStatus에 BEFORE_EXAM 추가

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/examRecord/entity/enums/ExamResultStatus.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/examRecord/entity/enums/ExamResultStatus.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Getter
 public enum ExamResultStatus {
+	BEFORE_EXAM("시험 응시 전"),
 	REVIEW_ONGOING("첨삭 진행 중"),
 	REVIEW_FINISH("첨삭 완료"),
 	RE_REVIEW_ONGOING("재첨삭 진행 중"),
