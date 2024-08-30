83e1fb7d08e2f039b762102b9cb601153214c5a2
sung-silver <jinna0319@gmail.com>
2024-08-30 15:01:29 +0900

    [FEAT] ExamRecord를 List 형태로 반환하도록 수정

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/examRecord/repository/ExamRecordRepository.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/examRecord/repository/ExamRecordRepository.java
@@ -2,7 +2,7 @@ package com.nonsoolmate.nonsoolmateServer.domain.examRecord.repository;
 
 import static com.nonsoolmate.nonsoolmateServer.domain.examRecord.exception.ExamRecordExceptionType.*;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -12,14 +12,10 @@ import com.nonsoolmate.nonsoolmateServer.domain.university.entity.Exam;
 import com.nonsoolmate.nonsoolmateServer.domain.university.exception.ExamException;
 
 public interface ExamRecordRepository extends JpaRepository<ExamRecord, Long> {
-	Optional<ExamRecord> findByExamAndMember(Exam university, Member member);
+	List<ExamRecord> findByExamAndMember(Exam university, Member member);
 
 	default ExamRecord findByExamAndMemberOrElseThrowException(Exam university, Member member) {
 		return findByExamAndMember(university, member).orElseThrow(
 			() -> new ExamException(NOT_FOUND_UNIVERSITY_EXAM_RECORD));
 	}
-
-	// TODO : 데모데이 이후 삭제 필요.
-	void deleteAllByMember(Member member);
-	// TODO : 데모데이 이후 삭제 필요.
 }
