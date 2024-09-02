98e6f8c0ae86fdf867f669c05a0c46e835b8463b
mikekks <thdalsrb79@naver.com>
2024-09-02 16:53:58 +0900

    [CHORE] 시험 조회 쿼리수 개선을 위해 IN 절로 변경

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/examRecord/repository/ExamRecordRepository.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/examRecord/repository/ExamRecordRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.nonsoolmate.nonsoolmateServer.domain.examRecord.entity.ExamRecord;
 import com.nonsoolmate.nonsoolmateServer.domain.examRecord.entity.enums.EditingType;
@@ -14,4 +15,11 @@ public interface ExamRecordRepository extends JpaRepository<ExamRecord, Long> {
 	List<ExamRecord> findByExamAndMember(Exam university, Member member);
 
 	Optional<ExamRecord> findByExamAndMemberAndEditingType(Exam exam, Member member, EditingType editingType);
+
+	@Query("SELECT r "
+		+ "FROM ExamRecord r "
+		+ "WHERE r.exam.examId IN :examIds "
+		+ "AND r.member.memberId = :memberId")
+	List<ExamRecord> findAllByExamIdInAndMemberId(List<Long> examIds, String memberId);
+
 }
