37e422605e060fc5ef14211f143ee3ba856677ba
mikekks <thdalsrb79@naver.com>
2024-09-02 16:53:28 +0900

    [CHORE] 시험 조회 쿼리수 개선을 위해 IN 절로 변경

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/repository/ExamRepository.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/university/repository/ExamRepository.java
@@ -4,12 +4,16 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import com.nonsoolmate.nonsoolmateServer.domain.university.entity.College;
 import com.nonsoolmate.nonsoolmateServer.domain.university.entity.Exam;
 
 public interface ExamRepository extends JpaRepository<Exam, Long> {
 	Optional<Exam> findByExamId(Long examId);
 
-	List<Exam> findAllByCollegeOrderByExamYearDesc(College college);
+	@Query("SELECT e "
+		+ "FROM Exam e "
+		+ "WHERE e.college.collegeId IN :collegeIds "
+		+ "ORDER BY e.examYear DESC ")
+	List<Exam> findAllByCollegeIdInOrderByExamYearDesc(List<Long> collegeIds);
 }
