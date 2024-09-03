8ec91c09cb3d81fa82a1ab53e2ffd14142983002
sung-silver <jinna0319@gmail.com>
2024-09-03 21:26:39 +0900

    [FEAT] EditingResultController 구현

---
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/examRecord/EditingResultController.java
@@ -0,0 +1,29 @@
+package com.nonsoolmate.nonsoolmateServer.domain.examRecord;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nonsoolmate.nonsoolmateServer.domain.examRecord.controller.dto.response.EditingResultDTO;
+import com.nonsoolmate.nonsoolmateServer.domain.examRecord.entity.enums.EditingType;
+import com.nonsoolmate.nonsoolmateServer.domain.examRecord.service.ExamRecordService;
+import com.nonsoolmate.nonsoolmateServer.domain.member.entity.Member;
+import com.nonsoolmate.nonsoolmateServer.global.security.AuthUser;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/college/exam")
+@RequiredArgsConstructor
+public class EditingResultController {
+	private final ExamRecordService examRecordService;
+
+	@GetMapping("/{exam-id}/exam-record/result")
+	public ResponseEntity<EditingResultDTO> getExamRecordResult(@PathVariable("exam-id") final long examId,
+		@RequestParam("type") final EditingType type, @AuthUser final Member member) {
+		return ResponseEntity.ok().body(examRecordService.getExamRecordEditingResult(examId, type, member));
+	}
+}
