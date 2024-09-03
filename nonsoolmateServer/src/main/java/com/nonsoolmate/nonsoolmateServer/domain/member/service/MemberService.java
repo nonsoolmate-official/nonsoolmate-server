457920c5a163b33b3fc37b31271dfb2320aba605
sung-silver <jinna0319@gmail.com>
2024-09-03 19:54:57 +0900

    [REFACTOR] getProfile에서 이미 조회된 member 객체로 정보 넘기도록 변경

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/member/service/MemberService.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/member/service/MemberService.java
@@ -7,15 +7,10 @@ import com.nonsoolmate.nonsoolmateServer.domain.member.controller.dto.response.N
 import com.nonsoolmate.nonsoolmateServer.domain.member.controller.dto.response.ProfileResponseDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.member.controller.dto.response.TicketResponseDTO;
 import com.nonsoolmate.nonsoolmateServer.domain.member.entity.Member;
-import com.nonsoolmate.nonsoolmateServer.domain.member.repository.MemberRepository;
-
-import lombok.RequiredArgsConstructor;
 
 @Service
 @Transactional(readOnly = true)
-@RequiredArgsConstructor
 public class MemberService {
-	private final MemberRepository memberRepository;
 
 	public NameResponseDTO getNickname(final Member member) {
 		return NameResponseDTO.of(member.getName());
@@ -26,9 +21,7 @@ public class MemberService {
 	}
 
 	public ProfileResponseDTO getProfile(final Member member) {
-		Member foundMember = memberRepository.findByMemberIdOrElseThrow(member.getMemberId());
-
-		return ProfileResponseDTO.of(foundMember.getName(), foundMember.getGender(), foundMember.getBirthYear(),
-			foundMember.getEmail(), foundMember.getPhoneNumber());
+		return ProfileResponseDTO.of(member.getName(), member.getGender(), member.getBirthYear(),
+			member.getEmail(), member.getPhoneNumber());
 	}
 }
