48b701a5b3d3ddbaf472f932aa163f288d3f1a20
sung-silver <jinna0319@gmail.com>
2024-09-03 21:24:25 +0900

    [FEAT] ExamRecordRepository - 첨삭 유형(첨삭, 재첨삭), 시험 id, 멤버 id를 바탕으로 examRecord를 반환하는 findByExamAndEditingType 구현

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/examRecord/repository/ExamRecordRepository.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/examRecord/repository/ExamRecordRepository.java
@@ -1,13 +1,17 @@
 package com.nonsoolmate.nonsoolmateServer.domain.examRecord.repository;
 
+import static com.nonsoolmate.nonsoolmateServer.domain.examRecord.exception.ExamRecordExceptionType.*;
+
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.nonsoolmate.nonsoolmateServer.domain.examRecord.entity.ExamRecord;
 import com.nonsoolmate.nonsoolmateServer.domain.examRecord.entity.enums.EditingType;
+import com.nonsoolmate.nonsoolmateServer.domain.examRecord.exception.ExamRecordException;
 import com.nonsoolmate.nonsoolmateServer.domain.member.entity.Member;
 import com.nonsoolmate.nonsoolmateServer.domain.university.entity.Exam;
 
@@ -22,4 +26,18 @@ public interface ExamRecordRepository extends JpaRepository<ExamRecord, Long> {
 		+ "AND r.member.memberId = :memberId")
 	List<ExamRecord> findAllByExamIdInAndMemberId(List<Long> examIds, String memberId);
 
+	@Query("SELECT r "
+		+ "FROM ExamRecord r "
+		+ "WHERE r.exam.examId = :examId "
+		+ "AND r.member.memberId = :memberId "
+		+ "AND r.editingType = :editingType")
+	Optional<ExamRecord> findByExamAndEditingType(
+		@Param("examId") Long examId,
+		@Param("editingType") EditingType editingType,
+		@Param("memberId") String memberId);
+
+	default ExamRecord findByExamAndMemberAndEditingTypeOrThrow(Long examId, EditingType editingType, String memberId) {
+		return findByExamAndEditingType(examId, editingType, memberId)
+			.orElseThrow(() -> new ExamRecordException(NOT_FOUND_EXAM_RECORD));
+	}
 }
