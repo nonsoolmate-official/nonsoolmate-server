5494903d0a130e11e856f60efccea198f5fc68fd
sung-silver <jinna0319@gmail.com>
2024-09-04 12:12:06 +0900

    [FEAT] File Url임을 더 명확하게 할 수 있도록 변수명 변경

--- nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/examRecord/controller/dto/response/EditingResultDTO.java
+++ nonsoolmateServer/src/main/java/com/nonsoolmate/nonsoolmateServer/domain/examRecord/controller/dto/response/EditingResultDTO.java
@@ -8,8 +8,8 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public record EditingResultDTO(
 	@Schema(description = "첨삭 유형(EDITING: 첨삭, REVISION: 재첨삭)", example = "EDITING") EditingType editingType,
 	@Schema(description = "해당 첨삭 진행 상태(첨삭 진행 중, 첨삭 완료, 재첨삭 진행 중, 재첨삭 완료)", example = "첨삭 진행 중") String examResultStatus,
-	@Schema(description = "첨삭 결과 파일 url", example = "https://cloud.nonsoolmate.com/filename") String examResultFile) {
-	public static EditingResultDTO of(EditingType editingType, String examResultStatus, String examResultFile) {
-		return new EditingResultDTO(editingType, examResultStatus, examResultFile);
+	@Schema(description = "첨삭 결과 파일 url", example = "https://cloud.nonsoolmate.com/filename") String examResultFileUrl) {
+	public static EditingResultDTO of(EditingType editingType, String examResultStatus, String examResultFileUrl) {
+		return new EditingResultDTO(editingType, examResultStatus, examResultFileUrl);
 	}
 }
