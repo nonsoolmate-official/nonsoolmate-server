54ed4f0fb327343b9a024c2998580fbcf513cc15
mikekks <thdalsrb79@naver.com>
2024-09-14 10:17:17 +0900

    [DEL] 사용하지 않는 코드 삭제

--- nonsoolmate-api/src/main/java/com/nonsoolmate/global/security/utils/RequestUtils.java
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/global/security/utils/RequestUtils.java
@@ -1,30 +1,16 @@
 package com.nonsoolmate.global.security.utils;
 
-
-import static com.nonsoolmate.exception.auth.AuthExceptionType.*;
-import static com.nonsoolmate.exception.auth.AuthSuccessType.*;
 import static org.springframework.http.HttpHeaders.*;
-
-import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Value;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.nonsoolmate.exception.auth.AuthExceptionType;
-import com.nonsoolmate.exception.auth.AuthSuccessType;
-import com.nonsoolmate.response.ErrorResponse;
-import com.nonsoolmate.response.SuccessResponse;
-
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
 public abstract class RequestUtils {
 
+	public static final String BEARER_HEADER = "Bearer ";
 	@Value("${jwt.access.header}")
 	private static String accessHeader = "Authorization";
-	public static final String BEARER_HEADER = "Bearer ";
-
-	private static final ObjectMapper objectMapper = new ObjectMapper();
 
 	public static boolean isContainsAccessToken(HttpServletRequest request) {
 		String authorization = request.getHeader(accessHeader);
@@ -36,31 +22,6 @@ public abstract class RequestUtils {
 	public static String getAuthorizationAccessToken(HttpServletRequest request) {
 		// "Bearer " 문자열 제외하고 뽑아오기
 		return request.getHeader(AUTHORIZATION).substring(7);
-	}
-
-	public static void setBodyOnResponse(HttpServletResponse response, AuthSuccessType success, Object bodyData) {
-		response.setStatus(LOGIN_SUCCESS.getHttpStatusCode());
-		response.setContentType("application/json");
-		response.setCharacterEncoding("utf-8");
-		try {
-			String body = objectMapper.writeValueAsString(SuccessResponse.of(success, bodyData));
-			response.getWriter().write(body);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public static void setErrorBodyOnResponse(HttpServletResponse response, AuthExceptionType error) {
-
-		response.setStatus(INVALID_ACCESS_TOKEN.getHttpStatusCode());
-		response.setContentType("application/json");
-		response.setCharacterEncoding("utf-8");
-		try {
-			String body = objectMapper.writeValueAsString(ErrorResponse.of(error));
-			response.getWriter().write(body);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 
 }
