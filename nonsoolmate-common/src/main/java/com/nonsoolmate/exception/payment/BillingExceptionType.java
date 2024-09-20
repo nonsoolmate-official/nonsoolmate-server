edceed15bfcdf727cf0923fa9cf3cfe2a0ef8119
sung-silver <jinna0319@gmail.com>
2024-09-20 17:04:09 +0900

    [FEAT] 등록된 카드가 없는 경우에 대한 예외 타입 구현

---
+++ nonsoolmate-common/src/main/java/com/nonsoolmate/exception/payment/BillingExceptionType.java
@@ -0,0 +1,26 @@
+package com.nonsoolmate.exception.payment;
+
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
+
+import com.nonsoolmate.exception.common.ExceptionType;
+
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum BillingExceptionType implements ExceptionType {
+	NOT_FOUND_BILLING(HttpStatus.NOT_FOUND, "사용자가 카드를 등록하지 않았습니다");
+
+	private final HttpStatus status;
+	private final String message;
+
+	@Override
+	public HttpStatus status() {
+		return this.status;
+	}
+
+	@Override
+	public String message() {
+		return this.message;
+	}
+}
