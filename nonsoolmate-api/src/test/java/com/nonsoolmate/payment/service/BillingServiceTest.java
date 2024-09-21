1d7648571cceb3a3fe5cf756dc73cb45aac5f616
sung-silver <jinna0319@gmail.com>
2024-09-21 22:15:02 +0900

    [TEST] 카드 정보에 대한 테스트 작성

---
+++ nonsoolmate-api/src/test/java/com/nonsoolmate/payment/service/BillingServiceTest.java
@@ -0,0 +1,78 @@
+package com.nonsoolmate.payment.service;
+
+import static com.nonsoolmate.exception.payment.BillingExceptionType.*;
+import static org.mockito.BDDMockito.*;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.nonsoolmate.exception.payment.BillingException;
+import com.nonsoolmate.member.entity.Member;
+import com.nonsoolmate.member.entity.enums.PlatformType;
+import com.nonsoolmate.member.entity.enums.Role;
+import com.nonsoolmate.payment.controller.dto.response.CardResponseDTO;
+import com.nonsoolmate.payment.entity.Billing;
+import com.nonsoolmate.payment.repository.BillingRepository;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class BillingServiceTest {
+	@Mock BillingRepository billingRepository;
+
+	@InjectMocks BillingService billingService;
+
+	private static final String memberId = "testMemberId";
+
+	@Test
+	@DisplayName("사용자가 등록한 카드를 확인하는 경우")
+	void getCardTestWhenMemberHasRegisteredCard() {
+		// given
+		Member expectedMember =
+				Member.builder()
+						.email("testEmail")
+						.name("testName")
+						.platformType(PlatformType.NAVER)
+						.platformId("testPlatformId")
+						.role(Role.USER)
+						.birthYear("test")
+						.build();
+		Billing expectedBilling =
+				Billing.builder()
+						.billingKey("testBillingKey")
+						.cardCompany("testCardCompany")
+						.cardNumber("testCardNumber")
+						.customer(expectedMember)
+						.build();
+		CardResponseDTO mockResponse =
+				CardResponseDTO.of(
+						expectedBilling.getBillingId(),
+						expectedBilling.getCardCompany(),
+						expectedBilling.getCardNumber());
+
+		given(billingRepository.findByCustomerIdOrThrow(anyString())).willReturn(expectedBilling);
+
+		// when
+		CardResponseDTO response = billingService.getCard(memberId);
+
+		// then
+		Assertions.assertThat(response).isEqualTo(mockResponse);
+	}
+
+	@Test
+	@DisplayName("사용자가 카드를 등록하지 않은 경우")
+	void getCardTestWhenMemberHasNotRegisteredCard() {
+		// given
+		given(billingRepository.findByCustomerIdOrThrow(anyString()))
+				.willThrow(new BillingException(NOT_FOUND_BILLING));
+
+		// when, then
+		Assertions.assertThatThrownBy(() -> billingService.getCard(memberId))
+				.isInstanceOf(BillingException.class)
+				.hasMessage("사용자가 카드를 등록하지 않았습니다");
+	}
+}
