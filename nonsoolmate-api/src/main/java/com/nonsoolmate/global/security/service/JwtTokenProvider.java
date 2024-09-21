11eaf9ba808d4099c75c09781540235a65d2f87b
mikekks <thdalsrb79@naver.com>
2024-09-21 17:51:00 +0900

    [FIX] 코드 스타일 변경

--- nonsoolmate-api/src/main/java/com/nonsoolmate/global/security/service/JwtTokenProvider.java
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/global/security/service/JwtTokenProvider.java
@@ -8,10 +8,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
