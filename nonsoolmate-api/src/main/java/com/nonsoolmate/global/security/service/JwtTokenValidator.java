47760245e704eb7ab81bef3a21bc4899d3a1fdb7
mikekks <thdalsrb79@naver.com>
2024-09-21 17:46:30 +0900

    [FIX] 코드 스타일 변경

--- nonsoolmate-api/src/main/java/com/nonsoolmate/global/security/service/JwtTokenValidator.java
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/global/security/service/JwtTokenValidator.java
@@ -18,34 +18,34 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 public class JwtTokenValidator {
 
-	public static final String BEARER_HEADER = "Bearer ";
+    public static final String BEARER_HEADER = "Bearer ";
 
-	@Value("${jwt.access.header}")
-	private static String accessHeader = "Authorization";
+    @Value("${jwt.access.header}")
+    private static String accessHeader = "Authorization";
 
-	private final Key key;
+    private final Key key;
 
-	public JwtTokenValidator(@Value("${jwt.secretKey}") String secretKey) {
-		byte[] keyBytes = Decoders.BASE64.decode(secretKey);
-		this.key = Keys.hmacShaKeyFor(keyBytes);
-	}
+    public JwtTokenValidator(@Value("${jwt.secretKey}") String secretKey) {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        this.key = Keys.hmacShaKeyFor(keyBytes);
+    }
 
-	public static boolean isContainsAccessToken(HttpServletRequest request) {
-		String authorization = request.getHeader(accessHeader);
-		return authorization != null && authorization.startsWith(BEARER_HEADER);
-	}
+    public static boolean isContainsAccessToken(HttpServletRequest request) {
+        String authorization = request.getHeader(accessHeader);
+        return authorization != null && authorization.startsWith(BEARER_HEADER);
+    }
 
-	// 유효한 Authorization Bearer Token에서 AccessToken 만 뽑아오기
-	public static String getAuthorizationAccessToken(HttpServletRequest request) {
-		// "Bearer " 문자열 제외하고 뽑아오기
-		return request.getHeader(AUTHORIZATION).substring(7);
-	}
+    // 유효한 Authorization Bearer Token에서 AccessToken 만 뽑아오기
+    public static String getAuthorizationAccessToken(HttpServletRequest request) {
+        // "Bearer " 문자열 제외하고 뽑아오기
+        return request.getHeader(AUTHORIZATION).substring(7);
+    }
 
-	public static String getMemberIdFromClaim(Claims claims, String infoName) {
-		return claims.get(infoName, String.class);
-	}
+    public static String getMemberIdFromClaim(Claims claims, String infoName) {
+        return claims.get(infoName, String.class);
+    }
 
-	public Claims getTokenClaims(final String token) {
-		return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
-	}
+    public Claims getTokenClaims(final String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
 }
