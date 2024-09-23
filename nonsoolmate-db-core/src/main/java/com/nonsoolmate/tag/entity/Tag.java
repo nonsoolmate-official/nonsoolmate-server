d7dd09d13259b4aa20ed9f132eb286657f678ebc
mikekks <thdalsrb79@naver.com>
2024-09-23 15:52:45 +0900

    [FIX] 코드 스타일 변경

--- nonsoolmate-db-core/src/main/java/com/nonsoolmate/tag/entity/Tag.java
+++ nonsoolmate-db-core/src/main/java/com/nonsoolmate/tag/entity/Tag.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,17 +14,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class Tag {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long tagId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long tagId;
 
-    @NotNull
-    private String tagName;
+	@NotNull private String tagName;
 
-    /**
-     * @implNote :
-     * 모든 테그 데이터가 해당 테이블에서 관리된다.
-     * 특정 도메인의 태그 데이터는 해당 컬럼이 null 이 아닌 row 를 조회한다.
-     * */
-    private Long teacherId;
+	/**
+	 * @implNote : 모든 테그 데이터가 해당 테이블에서 관리된다. 특정 도메인의 태그 데이터는 해당 컬럼이 null 이 아닌 row 를 조회한다.
+	 */
+	private Long teacherId;
 }
--- nonsoolmate-db-core/src/main/java/com/nonsoolmate/teacher/entity/TeacherUniversity.java
+++ nonsoolmate-db-core/src/main/java/com/nonsoolmate/teacher/entity/TeacherUniversity.java
@@ -1,28 +1,30 @@
 package com.nonsoolmate.teacher.entity;
 
-import com.nonsoolmate.university.entity.University;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import com.nonsoolmate.university.entity.University;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 public class TeacherUniversity {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long teacherUniversityId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long teacherUniversityId;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "teacher_id")
-    @NotNull
-    private Teacher teacher;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "teacher_id")
+	@NotNull
+	private Teacher teacher;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "university_id")
-    @NotNull
-    private University university;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "university_id")
+	@NotNull
+	private University university;
 }
