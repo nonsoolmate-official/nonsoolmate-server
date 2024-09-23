62815d40d9cbd78935940d1175d29c1bca9b7968
mikekks <thdalsrb79@naver.com>
2024-09-23 15:44:49 +0900

    [FEAT] 첨삭 선생님 관련 테이블 추가

---
+++ nonsoolmate-db-core/src/main/java/com/nonsoolmate/teacherMember/entity/TeacherMember.java
@@ -0,0 +1,30 @@
+package com.nonsoolmate.teacherMember.entity;
+
+
+import com.nonsoolmate.member.entity.Member;
+import com.nonsoolmate.teacher.entity.Teacher;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class TeacherMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long teacherMemberId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "teacher_id")
+    @NotNull
+    private Teacher teacher;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    @NotNull
+    private Member member;
+}
