f73a206b6255bfdfd31afc7b00f8a6875fd0a9d9
sung-silver <jinna0319@gmail.com>
2024-09-27 00:45:07 +0900

    [FEAT] 카드 등록 API 구현

--- nonsoolmate-api/src/main/java/com/nonsoolmate/payment/controller/CardController.java
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/payment/controller/CardController.java
@@ -1,13 +1,18 @@
 package com.nonsoolmate.payment.controller;
 
+import jakarta.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nonsoolmate.global.security.AuthMember;
+import com.nonsoolmate.payment.controller.dto.request.CreateCardRequestDTO;
 import com.nonsoolmate.payment.controller.dto.response.CardResponseDTO;
 import com.nonsoolmate.payment.service.BillingService;
 
@@ -20,6 +25,13 @@ public class CardController implements CardApi {
 	@GetMapping
 	public ResponseEntity<CardResponseDTO> getCard(@AuthMember final String memberId) {
 		CardResponseDTO response = billingService.getCard(memberId);
+		return ResponseEntity.ok(response);
+	}
+
+	@PostMapping("/register")
+	public ResponseEntity<CardResponseDTO> registerCard(
+			@RequestBody @Valid CreateCardRequestDTO createCardRequestDTO) {
+		CardResponseDTO response = billingService.registerCard(createCardRequestDTO);
 		return ResponseEntity.ok(response);
 	}
 }
