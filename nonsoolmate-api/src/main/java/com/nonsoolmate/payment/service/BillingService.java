22180658d52c42a783cb00975133d3a3da91cf1c
sung-silver <jinna0319@gmail.com>
2024-09-27 00:44:51 +0900

    [FEAT] BillingService에서 registerCard 구현

--- nonsoolmate-api/src/main/java/com/nonsoolmate/payment/service/BillingService.java
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/payment/service/BillingService.java
@@ -3,18 +3,48 @@ package com.nonsoolmate.payment.service;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.nonsoolmate.member.entity.Member;
+import com.nonsoolmate.member.repository.MemberRepository;
+import com.nonsoolmate.payment.controller.dto.request.CreateCardRequestDTO;
 import com.nonsoolmate.payment.controller.dto.response.CardResponseDTO;
 import com.nonsoolmate.payment.entity.Billing;
 import com.nonsoolmate.payment.repository.BillingRepository;
+import com.nonsoolmate.toss.service.TossPaymentService;
+import com.nonsoolmate.toss.service.vo.TossPaymentBillingVO;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class BillingService {
+	private final MemberRepository memberRepository;
 	private final BillingRepository billingRepository;
+	private final TossPaymentService tossPaymentService;
 
 	public CardResponseDTO getCard(final String memberId) {
 		Billing billing = billingRepository.findByCustomerIdOrThrow(memberId);
+		return CardResponseDTO.of(
+				billing.getBillingId(), billing.getCardCompany(), billing.getCardNumber());
+	}
+
+	@Transactional
+	public CardResponseDTO registerCard(final CreateCardRequestDTO request) {
+
+		TossPaymentBillingVO vo =
+				tossPaymentService.issueBilling(request.customerKey(), request.authKey());
+		Member customer = memberRepository.findByMemberIdOrThrow(request.customerKey());
+
+		Billing billing =
+				Billing.builder()
+						.customer(customer)
+						.billingKey(vo.billingKey())
+						.cardCompany(vo.cardCompany())
+						.cardNumber(vo.cardNumber())
+						.build();
+
+		billingRepository.save(billing);
+
 		return CardResponseDTO.of(
 				billing.getBillingId(), billing.getCardCompany(), billing.getCardNumber());
 	}
