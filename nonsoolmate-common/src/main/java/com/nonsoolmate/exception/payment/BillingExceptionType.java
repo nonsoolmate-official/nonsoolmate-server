a5eab219d2578851c36f6364caf5a7087c466434
sung-silver <jinna0319@gmail.com>
2024-09-27 02:11:38 +0900

    [FEAT] TOSS_PAYMENT_ISSUE_BILLING 예외 메세지 변경

--- nonsoolmate-common/src/main/java/com/nonsoolmate/exception/payment/BillingExceptionType.java
+++ nonsoolmate-common/src/main/java/com/nonsoolmate/exception/payment/BillingExceptionType.java
@@ -10,8 +10,7 @@ import com.nonsoolmate.exception.common.ExceptionType;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum BillingExceptionType implements ExceptionType {
 	NOT_FOUND_BILLING(HttpStatus.NOT_FOUND, "사용자가 카드를 등록하지 않았습니다"),
-	TOSS_PAYMENT_ISSUE_BILLING(HttpStatus.INTERNAL_SERVER_ERROR, "토스 결제 서버에서 카드 등록에 실패했습니다"),
-	;
+	TOSS_PAYMENT_ISSUE_BILLING(HttpStatus.BAD_REQUEST, "카드 등록에 실패했습니다");
 
 	private final HttpStatus status;
 	private final String message;
