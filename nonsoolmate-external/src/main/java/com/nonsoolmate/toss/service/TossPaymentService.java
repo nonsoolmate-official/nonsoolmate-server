9101981dc030f1e2550eca57f11b0376ea4ffe21
sung-silver <jinna0319@gmail.com>
2024-09-27 02:14:19 +0900

    [FEAT] toss API 연동 예외 분기 처리

--- nonsoolmate-external/src/main/java/com/nonsoolmate/toss/service/TossPaymentService.java
+++ nonsoolmate-external/src/main/java/com/nonsoolmate/toss/service/TossPaymentService.java
@@ -1,25 +1,40 @@
 package com.nonsoolmate.toss.service;
 
+import static com.nonsoolmate.exception.common.CommonErrorType.*;
 import static com.nonsoolmate.exception.payment.BillingExceptionType.*;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import com.nonsoolmate.exception.common.BusinessException;
+import com.nonsoolmate.exception.payment.BillingException;
 import com.nonsoolmate.toss.service.dto.request.IssueBillingDTO;
 import com.nonsoolmate.toss.service.dto.response.TossPaymentBillingDTO;
 import com.nonsoolmate.toss.service.vo.TossPaymentBillingVO;
 
+import reactor.core.publisher.Mono;
+
 @Service
 public class TossPaymentService {
 	private static final String TOSS_AUTHORIZATION_HEADER = "Authorization";
-	private static final String TOSS_AUTHORIZATION_PREFIX = "Basic";
+	private static final String TOSS_AUTHORIZATION_PREFIX = "Basic ";
 	private static final String TOSS_ISSUE_BILLING_URI =
 			"https://api.tosspayments.com/v1/billing/authorizations/issue";
+	private final String secretKey;
 
-	@Value("${toss.billing.api-key}")
-	private String accessToken;
+	public TossPaymentService(@Value("${toss.billing.api-key}") String apiKey) {
+		this.secretKey = getSecretKey(apiKey);
+	}
+
+	private String getSecretKey(String apiKey) {
+		return Base64.getEncoder().encodeToString(apiKey.getBytes(StandardCharsets.UTF_8));
+	}
 
 	public TossPaymentBillingVO issueBilling(final String customerKey, final String authKey) {
 		WebClient webClient = WebClient.builder().build();
@@ -30,9 +45,9 @@ public class TossPaymentService {
 					webClient
 							.post()
 							.uri(TOSS_ISSUE_BILLING_URI)
-							.header(TOSS_AUTHORIZATION_HEADER, TOSS_AUTHORIZATION_PREFIX + accessToken)
+							.header(TOSS_AUTHORIZATION_HEADER, TOSS_AUTHORIZATION_PREFIX + secretKey)
 							.header("Content-Type", "application/json")
-							.body(issueBillingDTO, IssueBillingDTO.class)
+							.body(Mono.just(issueBillingDTO), IssueBillingDTO.class)
 							.retrieve()
 							.bodyToMono(TossPaymentBillingDTO.class)
 							.block();
@@ -42,8 +57,16 @@ public class TossPaymentService {
 					tossPaymentBillingDTO.billingKey(),
 					tossPaymentBillingDTO.cardCompany(),
 					tossPaymentBillingDTO.cardNumber());
-		} catch (RuntimeException e) {
-			throw new BusinessException(TOSS_PAYMENT_ISSUE_BILLING);
+		} catch (WebClientResponseException e) {
+			boolean isUnauthorized = e.getStatusCode().equals(HttpStatus.UNAUTHORIZED);
+			boolean isNotFound = e.getStatusCode().equals(HttpStatus.NOT_FOUND);
+			if (isUnauthorized) {
+				throw new BusinessException(UNAUTHORIZED);
+			} else if (isNotFound) {
+				throw new BillingException(TOSS_PAYMENT_ISSUE_BILLING);
+			} else {
+				throw new BusinessException(EXTERNAL_SERVER_ERROR);
+			}
 		}
 	}
 }
