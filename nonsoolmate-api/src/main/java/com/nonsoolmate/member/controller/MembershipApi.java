71732f65f17935196c09c3b86deee38427a39128
sung-silver <jinna0319@gmail.com>
2024-09-27 16:32:27 +0900

    [FEAT] 멤버십 및 첨삭권 조회 API Swagger 명세 작성

---
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/member/controller/MembershipApi.java
@@ -0,0 +1,20 @@
+package com.nonsoolmate.member.controller;
+
+import org.springframework.http.ResponseEntity;
+
+import com.nonsoolmate.global.security.AuthMember;
+import com.nonsoolmate.member.controller.dto.response.MembershipAndTicketResponseDTO;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "membership", description = "멤버십과 관련된 API")
+public interface MembershipApi {
+	@ApiResponses(value = {@ApiResponse(responseCode = "200", description = "내 멤버십과 첨삭권 개수를 조회합니다")})
+	@Operation(summary = "모달: 내 멤버십과 첨삭권 개수 확인", description = "이름, 멤버십, 첨삭권 개수를 조회합니다")
+	ResponseEntity<MembershipAndTicketResponseDTO> getMembershipAndTicket(
+			@Parameter(hidden = true) @AuthMember String memberId);
+}
