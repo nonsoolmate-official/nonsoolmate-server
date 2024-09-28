08bba610086996e2f57ef1b8c6e3e06789f78091
sung-silver <jinna0319@gmail.com>
2024-09-29 00:36:12 +0900

    [FIX] CollegeName -> collegeName으로 변경

--- nonsoolmate-api/src/main/java/com/nonsoolmate/selectCollege/controller/dto/response/SelectCollegeExamsResponseDTO.java
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/selectCollege/controller/dto/response/SelectCollegeExamsResponseDTO.java
@@ -10,7 +10,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public record SelectCollegeExamsResponseDTO(
 		@Schema(description = "단과 대학 id", example = "1") Long collegeId,
 		@Schema(description = "대학 이름", example = "중앙대학교") String universityName,
-		@Schema(description = "단과 대학 이름", example = "경영경제") String CollegeName,
+		@Schema(description = "단과 대학 이름", example = "경영경제") String collegeName,
 		@Schema(description = "시험 리스트") List<SelectCollegeExamResponseDTO> examList) {
 	public static SelectCollegeExamsResponseDTO of(
 			final College college, final List<SelectCollegeExamResponseDTO> examList) {
