1ec776bdbdc5f283c5897f9bef110e07b69cdf4d
sung-silver <jinna0319@gmail.com>
2024-09-30 21:09:12 +0900

    [FEAT] 결제 API 명세서 작성

---
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/payment/controller/dto/response/PaymentResponseDTO.java
@@ -0,0 +1,10 @@
+package com.nonsoolmate.payment.controller.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "PaymentResponseDTO", description = "결제 요청에 대한 응답 DTO")
+public record PaymentResponseDTO(
+		/**
+		 * @note: paymentId = Transaction_Detail.transactionKey
+		 */
+		@Schema(description = "결제 완료 이후 반환되는 paymentId", example = "edslskdkdksl") String paymentId) {}
