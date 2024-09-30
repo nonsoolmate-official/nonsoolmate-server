4a25b8795be6c531b7778099bf42cf2e3c14945f
sung-silver <jinna0319@gmail.com>
2024-10-01 01:34:02 +0900

    [FEAT] productRepository에 findByProductIdOrThrow 구현

---
+++ nonsoolmate-common/src/main/java/com/nonsoolmate/exception/product/ProductException.java
@@ -0,0 +1,10 @@
+package com.nonsoolmate.exception.product;
+
+import com.nonsoolmate.exception.common.ClientException;
+import com.nonsoolmate.exception.common.ExceptionType;
+
+public class ProductException extends ClientException {
+	public ProductException(ExceptionType exceptionType) {
+		super(exceptionType);
+	}
+}
