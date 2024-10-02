04c08cac138e470a96d33eaefb4d851c328cf2c2
sung-silver <jinna0319@gmail.com>
2024-10-02 17:18:17 +0900

    [FEAT] 사용자가 결제 이력이 있는지 확인하는 메서드 구현

---
+++ nonsoolmate-db-core/src/main/java/com/nonsoolmate/payment/repository/TransactionDetailRepository.java
@@ -0,0 +1,9 @@
+package com.nonsoolmate.payment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nonsoolmate.payment.entity.TransactionDetail;
+
+public interface TransactionDetailRepository extends JpaRepository<TransactionDetail, String> {
+	boolean existsByCustomerKey(final String customerKey);
+}
