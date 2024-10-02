0ca950c998b59389ee7a10bc5241a9de66166f67
sung-silver <jinna0319@gmail.com>
2024-10-03 01:23:40 +0900

    [FEAT] createMembership 구현

--- nonsoolmate-api/src/main/java/com/nonsoolmate/member/service/MembershipService.java
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/member/service/MembershipService.java
@@ -7,9 +7,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.nonsoolmate.member.controller.dto.response.MembershipAndTicketResponseDTO;
 import com.nonsoolmate.member.entity.Member;
+import com.nonsoolmate.member.entity.Membership;
 import com.nonsoolmate.member.entity.enums.MembershipType;
 import com.nonsoolmate.member.repository.MemberRepository;
 import com.nonsoolmate.member.repository.MembershipRepository;
+import com.nonsoolmate.product.entity.Product;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +19,9 @@ import com.nonsoolmate.member.repository.MembershipRepository;
 public class MembershipService {
 	private final MemberRepository memberRepository;
 	private final MembershipRepository membershipRepository;
+
+	private final String BASIC_MEMBERSHIP_TYPE_NAME = "베이직 플랜";
+	private final String PREMIUM_MEMBERSHIP_TYPE_NAME = "프리미엄 플랜";
 
 	public MembershipAndTicketResponseDTO getMembershipAndTicket(final String memberId) {
 		Member member = memberRepository.findByMemberIdOrThrow(memberId);
@@ -38,5 +43,24 @@ public class MembershipService {
 		Member member = memberRepository.findByMemberIdOrThrow(memberId);
 		MembershipType membershipType = getMembershipType(member);
 		return membershipType != MembershipType.NONE;
+	}
+
+	public Membership createMembership(final String memberId, final Product product) {
+		Member member = memberRepository.findByMemberIdOrThrow(memberId);
+		MembershipType membershipType = getMembershipTypeByProduct(product);
+		Membership membership =
+				Membership.builder().member(member).membershipType(membershipType).build();
+		return membershipRepository.save(membership);
+	}
+
+	private MembershipType getMembershipTypeByProduct(final Product product) {
+		switch (product.getProductName()) {
+			case BASIC_MEMBERSHIP_TYPE_NAME:
+				return MembershipType.BASIC;
+			case PREMIUM_MEMBERSHIP_TYPE_NAME:
+				return MembershipType.PREMIUM;
+			default:
+				return MembershipType.NONE;
+		}
 	}
 }
