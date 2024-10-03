ccdf7d41ca25aa448da28d4a06887cf265b2c244
mikekks <thdalsrb79@naver.com>
2024-10-03 12:07:51 +0900

    [INFRA] 배치모듈 세팅

---
+++ nonsoolmate-batch/src/main/java/com/nonsoolmate/nonsoolmatebatch/NonsoolmateBatchApplication.java
@@ -0,0 +1,12 @@
+package com.nonsoolmate.nonsoolmatebatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NonsoolmateBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NonsoolmateBatchApplication.class, args);
+	}
+}
