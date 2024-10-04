a665360218cb8e9337ebec4fb5ffea455141c76f
mikekks <thdalsrb79@naver.com>
2024-10-04 16:45:40 +0900

    [CHORE] ApiResponses 추가

--- nonsoolmate-api/src/main/java/com/nonsoolmate/targetUniversity/controller/TargetUniversityApi.java
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/targetUniversity/controller/TargetUniversityApi.java
@@ -9,11 +9,17 @@ import com.nonsoolmate.targetUniversity.controller.dto.TargetUniversityResponseD
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
 @Tag(name = "TargetUniversity", description = "목표 대학과 관련된 API")
 public interface TargetUniversityApi {
 
+	@ApiResponses(
+			value = {
+				@ApiResponse(responseCode = "200", description = "설정 가능한 목표 대학 목록 조회에 성공했습니다."),
+			})
 	@Operation(summary = "(설정 가능한) 목표 대학 목록 조회", description = "설정 가능한 목표 대학 목록 조회 합니다.")
 	ResponseEntity<List<TargetUniversityResponseDto>> getTargetUniversities(
 			@Parameter(hidden = true) @AuthMember String memberId);
