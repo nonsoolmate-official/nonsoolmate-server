4b14381269880007cd9096dafde293983ae53e7e
sung-silver <jinna0319@gmail.com>
2024-10-04 15:23:58 +0900

    [FEAT] dto의 필드명 변경에 따른 수정

--- nonsoolmate-external/src/main/java/com/nonsoolmate/toss/service/vo/TossPaymentTransactionVO.java
+++ nonsoolmate-external/src/main/java/com/nonsoolmate/toss/service/vo/TossPaymentTransactionVO.java
@@ -6,12 +6,12 @@ import java.time.format.DateTimeFormatter;
 import com.nonsoolmate.toss.service.dto.response.TossPaymentTransactionDTO;
 
 public record TossPaymentTransactionVO(
-		String paymentKey, LocalDateTime transactionAt, String receiptUrl, String transactionKey) {
+		String transactionKey, String paymentKey, LocalDateTime transactionAt, String receiptUrl) {
 	public static TossPaymentTransactionVO fromDTO(TossPaymentTransactionDTO dto) {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
 		LocalDateTime transformedTransactionAt =
 				LocalDateTime.parse(dto.approvedAt().substring(0, 19), formatter);
 		return new TossPaymentTransactionVO(
-				dto.paymentKey(), transformedTransactionAt, dto.receipt().url(), dto.transactionKey());
+				dto.lastTransactionKey(), dto.paymentKey(), transformedTransactionAt, dto.receipt().url());
 	}
 }
