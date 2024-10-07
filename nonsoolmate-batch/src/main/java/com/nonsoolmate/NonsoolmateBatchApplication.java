0c8cac1005c7318ce03f2ce6cf21303e4813da23
mikekks <thdalsrb79@naver.com>
2024-10-07 17:24:00 +0900

    [FEAT] 빌링 자동 결제 스케줄러 구현

--- nonsoolmate-batch/src/main/java/com/nonsoolmate/NonsoolmateBatchApplication.java
+++ nonsoolmate-batch/src/main/java/com/nonsoolmate/NonsoolmateBatchApplication.java
@@ -1,9 +1,11 @@
-package com.nonsoolmate.nonsoolmatebatch;
+package com.nonsoolmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class NonsoolmateBatchApplication {
 
 	public static void main(String[] args) {
