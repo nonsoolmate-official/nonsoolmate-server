ff2b17d502d5ea7ee30335af59684005db6d0087
mikekks <thdalsrb79@naver.com>
2024-10-07 18:51:39 +0900

    [CHORE] @Async 적용

--- nonsoolmate-batch/src/main/java/com/nonsoolmate/payment/BillingScheduler.java
+++ nonsoolmate-batch/src/main/java/com/nonsoolmate/payment/BillingScheduler.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import lombok.RequiredArgsConstructor;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -64,32 +65,34 @@ public class BillingScheduler {
 
 		orders.stream()
 				.filter(orderDetail -> membershipMap.containsKey(orderDetail.getMember().getMemberId()))
-				.forEach(
-						order -> {
-							Member member = order.getMember();
-							Membership membership = membershipRepository.findByMemberOrThrow(member);
-							membership.validateMembershipStatus();
+				.forEach(order -> processBillingPayment(membershipMap, order));
+	}
 
-							String billingKey = getBillingKey(order.getMember().getMemberId());
-							String memberId = order.getMember().getMemberId();
+	@Async
+	public void processBillingPayment(Map<String, Membership> membershipMap, OrderDetail order) {
+		Member member = order.getMember();
+		Membership membership = membershipMap.get(member.getMemberId());
+		membership.validateMembershipStatus();
 
-							if (order.getCouponMember() != null) {
-								CouponMember couponMember = order.getCouponMember();
-								Coupon coupon = couponRepository.findByCouponIdOrThrow(couponMember.getCouponId());
-								long couponAppliedAmount = coupon.getCouponAppliedAmount(order.getAmount());
-								order.updateAmount(couponAppliedAmount);
-								couponMember.updateIsUsed(true);
-							}
+		String billingKey = getBillingKey(order.getMember().getMemberId());
+		String memberId = order.getMember().getMemberId();
 
-							tossPaymentService.requestBilling(billingKey, memberId, order);
+		if (order.getCouponMember() != null) {
+			CouponMember couponMember = order.getCouponMember();
+			Coupon coupon = couponRepository.findByCouponIdOrThrow(couponMember.getCouponId());
+			long couponAppliedAmount = coupon.getCouponAppliedAmount(order.getAmount());
+			order.updateAmount(couponAppliedAmount);
+			couponMember.updateIsUsed(true);
+		}
 
-							MembershipType membershipType =
-									MembershipType.getMembershipType(order.getProduct().getProductName());
-							membership.updateMembershipType(membershipType);
+		tossPaymentService.requestBilling(billingKey, memberId, order);
 
-							// create next month order
-							createOrder(order.getProduct(), NO_COUPON_MEMBER_ID, memberId);
-						});
+		MembershipType membershipType =
+				MembershipType.getMembershipType(order.getProduct().getProductName());
+		membership.updateMembershipType(membershipType);
+
+		// create next month order
+		createOrder(order.getProduct(), NO_COUPON_MEMBER_ID, memberId);
 	}
 
 	private String getBillingKey(final String customerKey) {
