d41301a0117b3f2fe6c5ca6a094d676025621bd4
sung-silver <jinna0319@gmail.com>
2024-10-07 14:09:23 +0900

    [FEAT] MembershipType에 description 및 getMembershipType 구현

--- nonsoolmate-db-core/src/main/java/com/nonsoolmate/member/entity/enums/MembershipType.java
+++ nonsoolmate-db-core/src/main/java/com/nonsoolmate/member/entity/enums/MembershipType.java
@@ -1,7 +1,26 @@
 package com.nonsoolmate.member.entity.enums;
 
+import static com.nonsoolmate.exception.member.MemberExceptionType.*;
+import static com.nonsoolmate.exception.member.MembershipExceptionType.*;
+
+import java.util.Arrays;
+
+import lombok.RequiredArgsConstructor;
+
+import com.nonsoolmate.exception.common.BusinessException;
+
+@RequiredArgsConstructor
 public enum MembershipType {
-	NONE,
-	BASIC,
-	PREMIUM;
+	NONE("멤버십 없음"),
+	BASIC("베이직 플랜"),
+	PREMIUM("프리미엄 플랜");
+
+	private final String description;
+
+	public static MembershipType getMembershipType(String description) {
+		return Arrays.stream(MembershipType.values())
+				.filter(type -> type.description.equals(description))
+				.findFirst()
+				.orElseThrow(() -> new BusinessException(NOT_FOUND_MEMBERSHIP_TYPE));
+	}
 }
