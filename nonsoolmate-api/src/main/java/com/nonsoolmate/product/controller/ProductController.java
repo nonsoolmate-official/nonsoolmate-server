662ff8721eddc93ff33faabee60432d2f19ef2f4
sung-silver <jinna0319@gmail.com>
2024-10-08 20:18:13 +0900

    [FEAT] 상품 조회 API 구현

---
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/product/controller/ProductController.java
@@ -0,0 +1,27 @@
+package com.nonsoolmate.product.controller;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nonsoolmate.global.security.AuthMember;
+import com.nonsoolmate.product.controller.dto.ProductResponseDTO;
+import com.nonsoolmate.product.service.ProductService;
+
+@RestController
+@RequestMapping("/products")
+@RequiredArgsConstructor
+public class ProductController implements ProductApi {
+	private final ProductService productService;
+
+	@GetMapping("/{productId}")
+	public ResponseEntity<ProductResponseDTO> getProduct(
+			@PathVariable("productId") final Long productId, @AuthMember final String memberId) {
+		ProductResponseDTO response = productService.getProduct(productId, memberId);
+		return ResponseEntity.ok(response);
+	}
+}
