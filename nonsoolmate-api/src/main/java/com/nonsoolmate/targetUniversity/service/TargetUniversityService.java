cd4b6ac5036d41e503b8f857360e4b910b569b5d
sung-silver <jinna0319@gmail.com>
2024-10-09 19:22:52 +0900

    [FEAT] TargetUniversityService에 patchTargetUniversity 구현

--- nonsoolmate-api/src/main/java/com/nonsoolmate/targetUniversity/service/TargetUniversityService.java
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/targetUniversity/service/TargetUniversityService.java
@@ -7,7 +7,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.nonsoolmate.member.entity.Member;
+import com.nonsoolmate.member.repository.MemberRepository;
 import com.nonsoolmate.targetUniversity.controller.dto.TargetUniversityResponseDto;
+import com.nonsoolmate.targetUniversity.entity.TargetUniversity;
 import com.nonsoolmate.targetUniversity.repository.TargetUniversityRepository;
 import com.nonsoolmate.university.entity.University;
 import com.nonsoolmate.university.repository.UniversityRepository;
@@ -18,10 +21,21 @@ import com.nonsoolmate.university.repository.UniversityRepository;
 public class TargetUniversityService {
 	private final UniversityRepository universityRepository;
 	private final TargetUniversityRepository targetUniversityRepository;
+	private final MemberRepository memberRepository;
 
 	public List<TargetUniversityResponseDto> getTargetUniversities() {
 		List<University> universities = universityRepository.findAll();
 
 		return universities.stream().map(TargetUniversityResponseDto::of).toList();
+	}
+
+	@Transactional
+	public void patchTargetUniversity(final List<Long> universityIds, final String memberId) {
+		Member member = memberRepository.findByMemberIdOrThrow(memberId);
+		targetUniversityRepository.deleteAllByMember(member);
+		List<University> universities = universityRepository.findAllByUniversityIdIn(universityIds);
+		List<TargetUniversity> targetUniversities =
+				universities.stream().map(university -> new TargetUniversity(university, member)).toList();
+		targetUniversityRepository.saveAll(targetUniversities);
 	}
 }
