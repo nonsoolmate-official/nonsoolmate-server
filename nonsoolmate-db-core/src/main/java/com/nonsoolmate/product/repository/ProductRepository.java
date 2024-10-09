2094788c1b3545d00d43c21e66f4f33de7384178
sung-silver <jinna0319@gmail.com>
2024-10-09 16:09:18 +0900

    [FEAT] 현재 판매 가능한 상품을 조회하는 findProductsByAvailable 구현

--- nonsoolmate-db-core/src/main/java/com/nonsoolmate/product/repository/ProductRepository.java
+++ nonsoolmate-db-core/src/main/java/com/nonsoolmate/product/repository/ProductRepository.java
@@ -2,9 +2,11 @@ package com.nonsoolmate.product.repository;
 
 import static com.nonsoolmate.exception.product.ProductExceptionType.*;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.nonsoolmate.exception.product.ProductException;
 import com.nonsoolmate.product.entity.Product;
@@ -15,4 +17,8 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 	default Product findByProductIdOrThrow(Long productId) {
 		return findByProductId(productId).orElseThrow(() -> new ProductException(INVALID_PRODUCT_ID));
 	}
+
+	@Query(
+			"select p from Product p where p.startDate <= current_timestamp and p.endDate >= current_timestamp")
+	List<Product> findProductsByAvailable();
 }
