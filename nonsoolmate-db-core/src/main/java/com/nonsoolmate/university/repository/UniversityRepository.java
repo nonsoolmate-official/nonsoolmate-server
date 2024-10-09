ada4a1702127c42df457e65613a1293b05fdcbcb
sung-silver <jinna0319@gmail.com>
2024-10-09 19:19:35 +0900

    [FEAT] UniversityRepository에 findAllByUniversityIdIn 구현

--- nonsoolmate-db-core/src/main/java/com/nonsoolmate/university/repository/UniversityRepository.java
+++ nonsoolmate-db-core/src/main/java/com/nonsoolmate/university/repository/UniversityRepository.java
@@ -1,7 +1,11 @@
 package com.nonsoolmate.university.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.nonsoolmate.university.entity.University;
 
-public interface UniversityRepository extends JpaRepository<University, Long> {}
+public interface UniversityRepository extends JpaRepository<University, Long> {
+	List<University> findAllByUniversityIdIn(List<Long> universityIds);
+}
