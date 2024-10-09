a3b551e2361b429095968da3867da6951a3c0196
sung-silver <jinna0319@gmail.com>
2024-10-09 16:06:02 +0900

    [FEAT] ProductResponseDTO에 productDescriptions 필드 추가

--- nonsoolmate-api/src/main/java/com/nonsoolmate/product/controller/dto/ProductResponseDTO.java
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/product/controller/dto/ProductResponseDTO.java
@@ -10,13 +10,16 @@ import io.swagger.v3.oas.annotations.media.Schema;
 public record ProductResponseDTO(
 		@Schema(description = "상품 id", example = "1") long productId,
 		@Schema(description = "상품 이름", example = "베이직 플랜") String productName,
+		@Schema(description = "상품 설명", example = "상품 설명 리스트") List<String> productDescriptions,
 		@Schema(description = "상품 원가", example = "100000") long price,
 		@Schema(description = "상품 기본 할인 정보") List<DiscountResponseDTO> defaultDiscounts) {
 	public static ProductResponseDTO of(
 			final Long productId,
 			final String productName,
+			final List<String> productDescriptions,
 			final long price,
 			final List<DiscountResponseDTO> defaultDiscounts) {
-		return new ProductResponseDTO(productId, productName, price, defaultDiscounts);
+		return new ProductResponseDTO(
+				productId, productName, productDescriptions, price, defaultDiscounts);
 	}
 }
