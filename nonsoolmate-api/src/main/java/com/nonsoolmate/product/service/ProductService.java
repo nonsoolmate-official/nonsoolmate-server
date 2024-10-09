153396bb4bc74459e84e40cddd2c0f1abb4240d2
sung-silver <jinna0319@gmail.com>
2024-10-09 16:11:28 +0900

    [FEAT] ProductService에 getProducts 구현 및 반복되는 로직 리팩터링

--- nonsoolmate-api/src/main/java/com/nonsoolmate/product/service/ProductService.java
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/product/service/ProductService.java
@@ -19,22 +19,49 @@ import com.nonsoolmate.product.repository.ProductRepository;
 @Transactional(readOnly = true)
 public class ProductService {
 	private final ProductRepository productRepository;
-	private final DiscountProductService productProductService;
+	private final DiscountProductService discountProductService;
 
 	public ProductResponseDTO getProduct(final Long productId, final String memberId) {
 		Product product = productRepository.findByProductIdOrThrow(productId);
-		List<DiscountProduct> defaultDiscountProducts =
-				productProductService.getDefaultDiscountProductsByProduct(product, memberId);
-		List<DiscountResponseDTO> defaultDiscounts =
-				defaultDiscountProducts.stream()
-						.map(
-								discountProduct ->
-										DiscountResponseDTO.of(
-												discountProduct.getDiscount().getDiscountId(),
-												discountProduct.getDiscount().getDiscountName(),
-												discountProduct.getDiscount().getDiscountRate()))
-						.toList();
+		List<DiscountProduct> discountProducts =
+				discountProductService.getDiscountProductsByProductAndMemberId(product, memberId);
+		List<DiscountResponseDTO> discountResponseDTOs =
+				convertToDiscountResponseDTOs(discountProducts);
 		return ProductResponseDTO.of(
-				product.getProductId(), product.getProductName(), product.getPrice(), defaultDiscounts);
+				product.getProductId(),
+				product.getProductName(),
+				product.getDescriptions(),
+				product.getPrice(),
+				discountResponseDTOs);
+	}
+
+	public List<ProductResponseDTO> getProducts() {
+		List<Product> products = productRepository.findProductsByAvailable();
+		return products.stream().map(this::createProductResponseDTO).toList();
+	}
+
+	private ProductResponseDTO createProductResponseDTO(Product product) {
+		List<DiscountProduct> discountProducts =
+				discountProductService.getDefaultDiscountProductsByProduct(product);
+		List<DiscountResponseDTO> discountResponseDTOs =
+				convertToDiscountResponseDTOs(discountProducts);
+		return ProductResponseDTO.of(
+				product.getProductId(),
+				product.getProductName(),
+				product.getDescriptions(),
+				product.getPrice(),
+				discountResponseDTOs);
+	}
+
+	private List<DiscountResponseDTO> convertToDiscountResponseDTOs(
+			List<DiscountProduct> discountProducts) {
+		return discountProducts.stream()
+				.map(
+						discountProduct ->
+								DiscountResponseDTO.of(
+										discountProduct.getDiscount().getDiscountId(),
+										discountProduct.getDiscount().getDiscountName(),
+										discountProduct.getDiscount().getDiscountRate()))
+				.toList();
 	}
 }
