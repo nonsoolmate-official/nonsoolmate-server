22698a525233d16f46c95a39ecebc14ca4a29a8b
mikekks <thdalsrb79@naver.com>
2024-10-09 18:03:56 +0900

    [ADD] 다음 달 결제 정보 조회 API 컨트롤러 구현

--- nonsoolmate-api/src/main/java/com/nonsoolmate/member/controller/MembershipApi.java
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/member/controller/MembershipApi.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 
 import com.nonsoolmate.global.security.AuthMember;
 import com.nonsoolmate.member.controller.dto.response.MembershipAndTicketResponseDTO;
+import com.nonsoolmate.member.controller.dto.response.PaymentInfoResponseDTO;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
@@ -16,5 +17,10 @@ public interface MembershipApi {
 	@ApiResponses(value = {@ApiResponse(responseCode = "200", description = "내 멤버십과 첨삭권 개수를 조회합니다")})
 	@Operation(summary = "모달: 내 멤버십과 첨삭권 개수 확인", description = "이름, 멤버십, 첨삭권 개수를 조회합니다")
 	ResponseEntity<MembershipAndTicketResponseDTO> getMembershipAndTicket(
+			@Parameter(hidden = true) @AuthMember String memberId);
+
+	@ApiResponses(value = {@ApiResponse(responseCode = "200", description = "다음달 결제 정보를 조회합니다.")})
+	@Operation(summary = "다음달 결제 정보", description = "다음달 결제 정보를 조회합니다.")
+	ResponseEntity<PaymentInfoResponseDTO> getNextPaymentInfo(
 			@Parameter(hidden = true) @AuthMember String memberId);
 }
