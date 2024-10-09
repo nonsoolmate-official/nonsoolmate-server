8122f502bcbe2b3e24512b2a026a7e056aa8ca8c
sung-silver <jinna0319@gmail.com>
2024-10-09 23:52:30 +0900

    [FEAT] BillingPaymentService에서 PaymentCommonService를 사용하도록 변경

--- nonsoolmate-batch/src/main/java/com/nonsoolmate/payment/BillingPaymentService.java
+++ nonsoolmate-batch/src/main/java/com/nonsoolmate/payment/BillingPaymentService.java
@@ -1,6 +1,5 @@
 package com.nonsoolmate.payment;
 
-import java.util.List;
 import java.util.Map;
 
 import lombok.RequiredArgsConstructor;
@@ -12,20 +11,11 @@ import org.springframework.transaction.annotation.Transactional;
 import com.nonsoolmate.coupon.entity.Coupon;
 import com.nonsoolmate.coupon.repository.CouponRepository;
 import com.nonsoolmate.couponMember.entity.CouponMember;
-import com.nonsoolmate.discount.entity.Discount;
-import com.nonsoolmate.discountProduct.entity.DiscountProduct;
-import com.nonsoolmate.discountProduct.repository.DiscountProductRepository;
 import com.nonsoolmate.member.entity.Member;
 import com.nonsoolmate.member.entity.Membership;
 import com.nonsoolmate.member.entity.enums.MembershipType;
-import com.nonsoolmate.member.repository.MemberRepository;
 import com.nonsoolmate.order.entity.OrderDetail;
-import com.nonsoolmate.order.repository.OrderRepository;
-import com.nonsoolmate.payment.entity.Billing;
-import com.nonsoolmate.payment.repository.BillingRepository;
-import com.nonsoolmate.payment.repository.TransactionDetailRepository;
-import com.nonsoolmate.product.entity.Product;
-import com.nonsoolmate.product.repository.ProductRepository;
+import com.nonsoolmate.service.PaymentCommonService;
 import com.nonsoolmate.toss.service.TossPaymentService;
 
 @Service
@@ -33,17 +23,11 @@ import com.nonsoolmate.toss.service.TossPaymentService;
 @Transactional(readOnly = true)
 public class BillingPaymentService {
   private static final Long NO_COUPON_MEMBER_ID = null;
-  private static final double NOT_FIRST_PURCHASE_DISCOUNT_RATE = 0.0;
 
-  private final BillingRepository billingRepository;
-  private final MemberRepository memberRepository;
-  private final ProductRepository productRepository;
-  private final DiscountProductRepository discountProductRepository;
-  private final TransactionDetailRepository transactionRepository;
   private final CouponRepository couponRepository;
-  private final OrderRepository orderRepository;
 
   private final TossPaymentService tossPaymentService;
+  private final PaymentCommonService paymentCommonService;
 
   @Async
   @Transactional
@@ -52,7 +36,7 @@ public class BillingPaymentService {
     Membership membership = membershipMap.get(member.getMemberId());
     membership.validateMembershipStatus();
 
-    String billingKey = getBillingKey(order.getMember().getMemberId());
+    String billingKey = paymentCommonService.getBillingKey(order.getMember().getMemberId());
     String memberId = order.getMember().getMemberId();
 
     if (order.getCouponMember() != null) {
@@ -70,58 +54,6 @@ public class BillingPaymentService {
     membership.updateMembershipType(membershipType);
 
     // create next month order
-    createOrder(order.getProduct(), NO_COUPON_MEMBER_ID, memberId);
-  }
-
-  private String getBillingKey(final String customerKey) {
-    Billing billing = billingRepository.findByCustomerIdOrThrow(customerKey);
-    return billing.getBillingKey();
-  }
-
-  private void createOrder(
-      final Product product, final Long couponMemberId, final String memberId) {
-    Member member = memberRepository.findByMemberIdOrThrow(memberId);
-    long discountedProductPrice = getDiscountedProductPrice(product.getProductId(), memberId);
-
-    OrderDetail order =
-        OrderDetail.builder()
-            .orderName(product.getProductName())
-            .member(member)
-            .product(product)
-            .amount(discountedProductPrice)
-            .build();
-
-    orderRepository.save(order);
-  }
-
-  private long getDiscountedProductPrice(final Long productId, final String memberId) {
-    Product product = productRepository.findByProductIdOrThrow(productId);
-    double discountedPrice = product.getPrice();
-
-    List<DiscountProduct> discountProducts = discountProductRepository.findAllByProduct(product);
-    boolean isFirstPurchaseMember = isFirstPurchase(memberId);
-
-    for (DiscountProduct discountProduct : discountProducts) {
-      Discount discount = discountProduct.getDiscount();
-      double discountPercent = getDiscountPercent(discount, isFirstPurchaseMember);
-      discountedPrice *= (1.0 - discountPercent);
-    }
-
-    return Math.round(discountedPrice);
-  }
-
-  private boolean isFirstPurchase(final String memberId) {
-    return !transactionRepository.existsByCustomerKey(memberId);
-  }
-
-  private double getDiscountPercent(final Discount discount, final boolean isFirstPurchaseMember) {
-    switch (discount.getDiscountType()) {
-      case FIRST_PURCHASE:
-        return isFirstPurchaseMember
-            ? discount.getDiscountRate()
-            : NOT_FIRST_PURCHASE_DISCOUNT_RATE;
-      default:
-        return discount.getDiscountRate();
-    }
+    paymentCommonService.createOrder(order.getProduct(), NO_COUPON_MEMBER_ID, memberId);
   }
 }
