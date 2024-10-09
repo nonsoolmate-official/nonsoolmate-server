13ad5886fae6bd9582264e4f601ee7de2203231b
sung-silver <jinna0319@gmail.com>
2024-10-09 19:50:04 +0900

    [FEAT] 목표 대학 선택 API 구현

--- nonsoolmate-api/src/main/java/com/nonsoolmate/targetUniversity/controller/TargetUniversityController.java
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/targetUniversity/controller/TargetUniversityController.java
@@ -2,14 +2,19 @@ package com.nonsoolmate.targetUniversity.controller;
 
 import java.util.List;
 
+import jakarta.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nonsoolmate.global.security.AuthMember;
+import com.nonsoolmate.targetUniversity.controller.dto.request.TargetUniversityRequestDTO;
 import com.nonsoolmate.targetUniversity.controller.dto.response.TargetUniversityResponseDTO;
 import com.nonsoolmate.targetUniversity.service.TargetUniversityService;
 
@@ -25,5 +30,14 @@ public class TargetUniversityController implements TargetUniversityApi {
 			@AuthMember String memberId) {
 
 		return ResponseEntity.ok().body(targetUniversityService.getTargetUniversities());
+	}
+
+	@PatchMapping("/select")
+	@Override
+	public ResponseEntity<Void> patchTargetUniversities(
+			@AuthMember final String memberId,
+			@Valid @RequestBody final List<TargetUniversityRequestDTO> universityIds) {
+		targetUniversityService.patchTargetUniversity(universityIds, memberId);
+		return ResponseEntity.ok().build();
 	}
 }
