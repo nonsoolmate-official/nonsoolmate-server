898fc18af0f258cd7907a6e477d1c21ce7fbee12
sung-silver <jinna0319@gmail.com>
2024-10-09 19:21:46 +0900

    [FEAT] TargetUniversityRepository에 deleteAllByMember 구현

--- nonsoolmate-db-core/src/main/java/com/nonsoolmate/targetUniversity/repository/TargetUniversityRepository.java
+++ nonsoolmate-db-core/src/main/java/com/nonsoolmate/targetUniversity/repository/TargetUniversityRepository.java
@@ -1,7 +1,17 @@
 package com.nonsoolmate.targetUniversity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.nonsoolmate.member.entity.Member;
 import com.nonsoolmate.targetUniversity.entity.TargetUniversity;
 
-public interface TargetUniversityRepository extends JpaRepository<TargetUniversity, Long> {}
+public interface TargetUniversityRepository extends JpaRepository<TargetUniversity, Long> {
+	@Modifying(clearAutomatically = true)
+	@Transactional
+	@Query("DELETE FROM TargetUniversity tu WHERE tu.member = :member")
+	void deleteAllByMember(@Param("member") Member member);
+}
