06b2c4e99848eba635ddc6c91f6d405072bc3c11
sung-silver <jinna0319@gmail.com>
2024-10-09 19:40:50 +0900

    [FEAT] 목표 대학 선택 API 명세서 작성

--- nonsoolmate-api/src/main/java/com/nonsoolmate/targetUniversity/controller/TargetUniversityApi.java
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/targetUniversity/controller/TargetUniversityApi.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 
 import com.nonsoolmate.global.security.AuthMember;
+import com.nonsoolmate.targetUniversity.controller.dto.request.TargetUniversityRequestDTO;
 import com.nonsoolmate.targetUniversity.controller.dto.response.TargetUniversityResponseDTO;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -23,4 +24,12 @@ public interface TargetUniversityApi {
 	@Operation(summary = "(설정 가능한) 목표 대학 목록 조회", description = "설정 가능한 목표 대학 목록 조회 합니다.")
 	ResponseEntity<List<TargetUniversityResponseDTO>> getTargetUniversities(
 			@Parameter(hidden = true) @AuthMember String memberId);
+
+	@ApiResponses(
+			value = {
+				@ApiResponse(responseCode = "200", description = "목표 대학 설정에 성공하였습니다."),
+			})
+	@Operation(summary = "목표 대학 설정: 리스트 선택", description = "내 목표 대학들 리스트를 설정합니다.")
+	ResponseEntity<Void> patchTargetUniversities(
+			@Parameter(hidden = true) String memberId, List<TargetUniversityRequestDTO> request);
 }
