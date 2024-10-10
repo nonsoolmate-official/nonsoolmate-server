80ac9e60e597a844892735b868aefa2d0337369b
sung-silver <jinna0319@gmail.com>
2024-10-10 22:09:01 +0900

    [FEAT] MembershipExceptionType에 NOT_FOUND_MEMBERSHIP 추가

--- nonsoolmate-exception/src/main/java/com/nonsoolmate/exception/member/MembershipExceptionType.java
+++ nonsoolmate-exception/src/main/java/com/nonsoolmate/exception/member/MembershipExceptionType.java
@@ -10,6 +10,7 @@ import com.nonsoolmate.exception.common.ExceptionType;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum MembershipExceptionType implements ExceptionType {
   NOT_FOUND_MEMBERSHIP_TYPE(HttpStatus.NOT_FOUND, "존재하지 않는 멤버십 타입입니다."),
+  NOT_FOUND_MEMBERSHIP(HttpStatus.NOT_FOUND, "멤버십이 존재하지 않습니다."),
   TERMINATED_MEMBERSHIP(HttpStatus.BAD_REQUEST, "이미 해지된 멤버십 입니다.");
 
   private final HttpStatus status;
