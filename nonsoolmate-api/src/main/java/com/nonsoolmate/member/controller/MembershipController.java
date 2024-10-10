9d06b19ec25c6437e44e3c61bf0a9521dd2373f2
sung-silver <jinna0319@gmail.com>
2024-10-10 22:10:50 +0900

    [FEAT] 멤버십 상태 변경 API 구현

--- nonsoolmate-api/src/main/java/com/nonsoolmate/member/controller/MembershipController.java
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/member/controller/MembershipController.java
@@ -2,16 +2,22 @@ package com.nonsoolmate.member.controller;
 
 import java.util.Optional;
 
+import jakarta.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nonsoolmate.global.security.AuthMember;
+import com.nonsoolmate.member.controller.dto.request.MembershipStatusRequestDTO;
 import com.nonsoolmate.member.controller.dto.response.MembershipAndTicketResponseDTO;
+import com.nonsoolmate.member.controller.dto.response.MembershipStatusResponseDTO;
 import com.nonsoolmate.member.controller.dto.response.PaymentInfoResponseDTO;
 import com.nonsoolmate.member.service.MembershipService;
 
@@ -41,5 +47,15 @@ public class MembershipController implements MembershipApi {
     }
 
     return ResponseEntity.ok().body(nextPaymentInfo.get());
+  }
+
+  @Override
+  @PatchMapping("/status")
+  public ResponseEntity<MembershipStatusResponseDTO> changeMembershipStatus(
+      @AuthMember final String memberId,
+      @Valid @RequestBody final MembershipStatusRequestDTO request) {
+    MembershipStatusResponseDTO response =
+        membershipService.changeMembershipStatus(memberId, request);
+    return ResponseEntity.ok().body(response);
   }
 }
