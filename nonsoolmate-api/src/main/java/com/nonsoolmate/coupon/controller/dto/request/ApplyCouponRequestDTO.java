df0f210d3c8bb685a64173e9c90879dcf6d74823
mikekks <thdalsrb79@naver.com>
2024-10-11 15:50:27 +0900

    [FEAT] 쿠폰 사용 API 컨트롤러 구현

---
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/coupon/controller/dto/request/ApplyCouponRequestDTO.java
@@ -0,0 +1,10 @@
+package com.nonsoolmate.coupon.controller.dto.request;
+
+import jakarta.validation.constraints.NotNull;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "ApplyCouponRequestDTO", description = "쿠폰 적용 요청 DTO")
+public record ApplyCouponRequestDTO(
+    @NotNull @Schema(description = "쿠폰 id", example = "3") Long couponMemberId,
+    @NotNull @Schema(description = "주문 id", example = "3") Long orderId) {}
