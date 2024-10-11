a4a95de11a2011bfcd45b385784427bf2d861ca3
mikekks <thdalsrb79@naver.com>
2024-10-11 16:00:29 +0900

    [CHORE] 필요없는 필드 삭제

--- nonsoolmate-api/src/main/java/com/nonsoolmate/coupon/controller/dto/request/ApplyCouponRequestDTO.java
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/coupon/controller/dto/request/ApplyCouponRequestDTO.java
@@ -6,5 +6,4 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(name = "ApplyCouponRequestDTO", description = "쿠폰 적용 요청 DTO")
 public record ApplyCouponRequestDTO(
-    @NotNull @Schema(description = "쿠폰 id", example = "3") Long couponMemberId,
-    @NotNull @Schema(description = "주문 id", example = "3") Long orderId) {}
+    @NotNull @Schema(description = "쿠폰 id", example = "3") Long couponMemberId) {}
