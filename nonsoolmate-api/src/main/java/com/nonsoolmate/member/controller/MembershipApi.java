52c872fdd125fde73792668881c7d326f901e318
sung-silver <jinna0319@gmail.com>
2024-10-12 15:19:05 +0900

    [FIX] 멤버십 조회 swagger 설명 변경

--- nonsoolmate-api/src/main/java/com/nonsoolmate/member/controller/MembershipApi.java
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/member/controller/MembershipApi.java
@@ -44,7 +44,7 @@ public interface MembershipApi {
         @ApiResponse(responseCode = "200", description = "멤버십 조회에 성공한 경우"),
         @ApiResponse(responseCode = "204", description = "멤버십 구매를 하지 않았는데 멤버십 조회를 요청하는 경우")
       })
-  @Operation(summary = "멤버십 상태 변경", description = "멤버십 상태를 변경합니다.")
+  @Operation(summary = "멤버십 조회", description = "멤버십 상태를 조회합니다")
   ResponseEntity<MembershipStatusResponseDTO> getMembership(
       @Parameter(hidden = true) @AuthMember String memberId);
 }
