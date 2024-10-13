d007b2c56fb1b6552013225a5df729e02752b521
sung-silver <jinna0319@gmail.com>
2024-10-13 22:23:14 +0900

    [FEAT] CreatExamRecordRequestDTO 예시 응답값 수정

--- nonsoolmate-api/src/main/java/com/nonsoolmate/examRecord/controller/dto/request/CreateExamRecordRequestDTO.java
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/examRecord/controller/dto/request/CreateExamRecordRequestDTO.java
@@ -12,7 +12,7 @@ public record CreateExamRecordRequestDTO(
     @NotNull @Schema(description = "대학 시험 id", example = "1") Long examId,
     @Positive @Schema(description = "사용자가 해당 시험을 보는데 걸린 시간(초 단위)") int memberTakeTimeExam,
     @NotNull @Schema(description = "PreSingedUrl 발급 시 전달 받은 파일 이름") String memberSheetFileName,
-    @NotNull @Schema(description = "첨삭 유형(EDITING, REVISION)", example = "REVIEW")
+    @NotNull @Schema(description = "첨삭 유형(EDITING, REVISION)", example = "REVISION")
         EditingType editingType) {
   public static CreateExamRecordRequestDTO of(
       Long examId, int memberTakeTimeExam, String memberSheetFileName, EditingType editingType) {
