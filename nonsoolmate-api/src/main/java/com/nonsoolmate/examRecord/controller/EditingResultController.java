46fe0e746151a7f109b8a89877b7d35bce76d014
sung-silver <jinna0319@gmail.com>
2024-10-13 22:54:03 +0900

    [FEAT] 첨삭 결과 반영 API 구현

--- nonsoolmate-api/src/main/java/com/nonsoolmate/examRecord/controller/EditingResultController.java
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/examRecord/controller/EditingResultController.java
@@ -1,14 +1,19 @@
 package com.nonsoolmate.examRecord.controller;
 
+import jakarta.validation.Valid;
+
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.nonsoolmate.examRecord.controller.dto.request.UpdateExamRecordResultRequestDTO;
 import com.nonsoolmate.examRecord.controller.dto.response.EditingResultDTO;
 import com.nonsoolmate.examRecord.entity.enums.EditingType;
 import com.nonsoolmate.examRecord.service.ExamRecordService;
@@ -27,5 +32,12 @@ public class EditingResultController implements EditingResultApi {
       @AuthMember final String memberId) {
     return ResponseEntity.ok()
         .body(examRecordService.getExamRecordEditingResult(examId, type, memberId));
+  }
+
+  // for admin
+  @PatchMapping("/result")
+  public ResponseEntity<EditingResultDTO> updateExamRecordResult(
+      @Valid @RequestBody final UpdateExamRecordResultRequestDTO request) {
+    return ResponseEntity.ok().body(examRecordService.updateExamRecordEditingResult(request));
   }
 }
