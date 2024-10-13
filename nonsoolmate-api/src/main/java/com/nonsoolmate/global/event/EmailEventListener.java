e72f7289a0f7b081de4787f3e0e61a9b502c19b5
sung-silver <jinna0319@gmail.com>
2024-10-14 00:13:13 +0900

    [FEAT] Email 관련 이벤트 리스너 구현

---
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/global/event/EmailEventListener.java
@@ -0,0 +1,21 @@
+package com.nonsoolmate.global.event;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import com.nonsoolmate.email.service.EmailService;
+
+@Component
+@RequiredArgsConstructor
+public class EmailEventListener {
+  private final EmailService emailService;
+
+  @Async
+  @EventListener
+  public void publishExamRecordStatusUpdatedEvent(ExamRecordStatusUpdatedEvent event) {
+    emailService.sendMessage(event.email());
+  }
+}
