829642e352996d4fc32b5ffa6db98e5166916bd8
sung-silver <jinna0319@gmail.com>
2024-10-14 00:11:20 +0900

    [FEAT] AsyncConfig 구현

---
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/global/config/AsyncConfig.java
@@ -0,0 +1,8 @@
+package com.nonsoolmate.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {}
