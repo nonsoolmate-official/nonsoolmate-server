73354867a6c8804e64784e3199664e0661bfca54
sung-silver <jinna0319@gmail.com>
2024-10-14 13:40:52 +0900

    [FEAT] 이벤트 리스터 TransactionalEventListener로 변경

--- nonsoolmate-api/src/main/java/com/nonsoolmate/global/event/EmailEventListener.java
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/global/event/EmailEventListener.java
@@ -2,9 +2,10 @@ package com.nonsoolmate.global.event;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import com.nonsoolmate.email.service.EmailService;
 
@@ -14,7 +15,7 @@ public class EmailEventListener {
   private final EmailService emailService;
 
   @Async
-  @EventListener
+  @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
   public void publishExamRecordStatusUpdatedEvent(ExamRecordStatusUpdatedEvent event) {
     emailService.sendMessageAboutExamRecordStatus(
         event.email(), event.editingType(), event.examFullName());
