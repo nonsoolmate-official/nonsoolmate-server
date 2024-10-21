910c5185a104cb874c12ed44500bd361d4354a8b
sung-silver <jinna0319@gmail.com>
2024-10-21 14:39:02 +0900

    [FEAT] TargetUniversityResponseDTO University 객체가 아닌 각 파라미터를 받도록 변경

--- nonsoolmate-api/src/main/java/com/nonsoolmate/targetUniversity/controller/dto/response/TargetUniversityResponseDTO.java
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/targetUniversity/controller/dto/response/TargetUniversityResponseDTO.java
@@ -1,7 +1,5 @@
 package com.nonsoolmate.targetUniversity.controller.dto.response;
 
-import com.nonsoolmate.university.entity.University;
-
 import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(name = "TargetUniversityResponseDTO", description = "목표 대학 조회 응답 DTO")
@@ -10,10 +8,8 @@ public record TargetUniversityResponseDTO(
     @Schema(description = "대학 이름", example = "중앙대학교") String universityName,
     @Schema(description = "대학 이미지 url", example = "[url 형식]") String universityImageUrl) {
 
-  public static TargetUniversityResponseDTO of(University university) {
-    return new TargetUniversityResponseDTO(
-        university.getUniversityId(),
-        university.getUniversityName(),
-        university.getUniversityImageUrl());
+  public static TargetUniversityResponseDTO of(
+      Long universityId, String universityName, String universityImageUrl) {
+    return new TargetUniversityResponseDTO(universityId, universityName, universityImageUrl);
   }
 }
