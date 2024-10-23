418ea41d0c76d54a183de12a53ad99b417db54ba
sung-silver <jinna0319@gmail.com>
2024-10-23 17:30:58 +0900

    [FEAT] teacher response DTO 분리 및 대학교 이미지를 cloudfront를 사용하여 가져오도록 변경"

--- nonsoolmate-api/src/main/java/com/nonsoolmate/member/service/MemberService.java
+++ nonsoolmate-api/src/main/java/com/nonsoolmate/member/service/MemberService.java
@@ -1,5 +1,8 @@
 package com.nonsoolmate.member.service;
 
+import static com.nonsoolmate.aws.FolderName.UNIVERSITY_IMAGE_FOLDER_NAME;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,12 +11,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.nonsoolmate.aws.service.CloudFrontService;
 import com.nonsoolmate.matching.entity.Matching;
 import com.nonsoolmate.matching.repository.MatchingRepository;
 import com.nonsoolmate.member.controller.dto.request.ProfileRequestDTO;
 import com.nonsoolmate.member.controller.dto.response.NameResponseDTO;
 import com.nonsoolmate.member.controller.dto.response.ProfileResponseDTO;
 import com.nonsoolmate.member.controller.dto.response.TeacherResponseDTO;
+import com.nonsoolmate.member.controller.dto.response.TeacherUniversityDTO;
 import com.nonsoolmate.member.entity.Member;
 import com.nonsoolmate.member.repository.MemberRepository;
 import com.nonsoolmate.payment.controller.dto.response.CustomerInfoDTO;
@@ -22,6 +27,7 @@ import com.nonsoolmate.tag.repository.TagRepository;
 import com.nonsoolmate.teacher.entity.Teacher;
 import com.nonsoolmate.teacher.entity.TeacherUniversity;
 import com.nonsoolmate.teacher.repository.TeacherUniversityRepository;
+import com.nonsoolmate.university.entity.University;
 
 @Service
 @RequiredArgsConstructor
@@ -32,6 +38,7 @@ public class MemberService {
   private final MatchingRepository matchingRepository;
   private final TagRepository tagRepository;
   private final TeacherUniversityRepository teacherUniversityRepository;
+  private final CloudFrontService cloudFrontService;
 
   public NameResponseDTO getNickname(final String memberId) {
     Member member = memberRepository.findByMemberIdOrThrow(memberId);
@@ -68,7 +75,6 @@ public class MemberService {
   }
 
   public Optional<TeacherResponseDTO> getMyTeacher(final String memberId) {
-
     Optional<Matching> foundMatching =
         matchingRepository.findByMemberIdWithTeacherAndMember(memberId);
 
@@ -85,8 +91,23 @@ public class MemberService {
     Teacher teacher = matching.getTeacher();
     List<TeacherUniversity> teacherUniversities =
         teacherUniversityRepository.findAllByTeacher(teacher);
+    List<TeacherUniversityDTO> teacherUniversityDTOs = new ArrayList<>();
+
+    for (TeacherUniversity teacherUniversity : teacherUniversities) {
+      University university = teacherUniversity.getUniversity();
+      teacherUniversityDTOs.add(getTeacherUniversityDTO(university));
+    }
+
     List<Tag> tags = tagRepository.findAllByTeacherId(teacher.getTeacherId());
 
-    return Optional.of(TeacherResponseDTO.of(true, teacher, teacherUniversities, tags));
+    return Optional.of(TeacherResponseDTO.of(true, teacher, teacherUniversityDTOs, tags));
+  }
+
+  private TeacherUniversityDTO getTeacherUniversityDTO(final University university) {
+    String universityName = university.getUniversityName();
+    String universityImageName = university.getUniversityImageUrl();
+    String universityImageUrl =
+        cloudFrontService.createPreSignedGetUrl(UNIVERSITY_IMAGE_FOLDER_NAME, universityImageName);
+    return TeacherUniversityDTO.of(universityName, universityImageUrl);
   }
 }
